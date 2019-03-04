7c31a772776618c59c58ca5aa7fed7853b94a5a5
tienthanh <tienthanh.nguyen@tum.de>
2019-03-04 11:21:51 -0500

    add new file for shared variables

---
+++ DataModel.f90
@@ -0,0 +1,24 @@
+module DataModel
+
+    implicit none
+    INTEGER :: ios, fh
+    
+    !double precision :: wct_start,wct_end,cput_start,cput_end,runtime
+
+    !INTEGER :: mi, ma
+    !INTEGER :: num_of_threads
+    
+
+    !integer :: ierr
+    !integer :: num_procs
+    !integer :: rank
+    
+    INTEGER :: N = 1140404
+    INTEGER :: M = 1140407
+    INTEGER :: trial = 1000
+
+    !INTEGER :: c = 0
+    !Length = 300*1000
+    INTEGER :: Length = 12 * 100000
+    contains
+end module DataModel
