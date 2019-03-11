f0f794782da667d07370e1eb5a82afd1f1b58fc3
nguyentienthanh812 <thanhnt812@yahoo.com>
2019-03-12 02:49:59 +0700

    add new variables for # of loops

--- DataModel.f90
+++ DataModel.f90
@@ -3,23 +3,25 @@ module DataModel
     implicit none
     INTEGER :: ios, fh
     
-    !double precision :: wct_start,wct_end,cput_start,cput_end,runtime
-
-    !INTEGER :: mi, ma
-    !INTEGER :: num_of_threads
     
-
-    !integer :: ierr
-    !integer :: num_procs
-    !integer :: rank
     
     INTEGER :: N = 1140404
     INTEGER :: M = 1140407
-    INTEGER :: trial = 1000
+    INTEGER :: Length = 12 * 100000
+
+    integer :: N_Length = 800
+
+    !INTEGER :: N = 1000
+    !INTEGER :: M = 1007
+    !INTEGER :: Length = 12 * 100
+
+    INTEGER :: trial = 1000*1000
+
+    INTEGER :: N_Loop = 800
 
     !INTEGER :: c = 0
     !Length = 300*1000
-    INTEGER :: Length = 12 * 100000
+    
 
     integer :: chunk_size
     contains
