8f4b1cef3971de006cbbbfe1b1f7ffee44189acf
tienthanh <tienthanh.nguyen@tum.de>
2019-03-12 11:50:49 -0400

    new subroutine for loading spare matrix VAL, COL

--- ReadFile.f90
+++ ReadFile.f90
@@ -98,4 +98,107 @@ SUBROUTINE readINT(ios, fh, X, fn)
     CLOSE(fh)
 ENDSUBROUTINE
 
+SUBROUTINE LoadIndexes(L1, L1_1, L2, L2_1, VAL_1, VAL_2, COL_1, COL_2, row1, row2, G1, G2, ma, mi)
+    IMPLICIT NONE
+    INTEGER, DIMENSION (:), ALLOCATABLE, INTENT(INOUT) :: L1, L1_1, L2, L2_1
+    INTEGER, DIMENSION (:), ALLOCATABLE, INTENT(INOUT) :: VAL_1, VAL_2, COL_1, COL_2
+    INTEGER, DIMENSION (:), ALLOCATABLE, INTENT(INOUT) :: row1, row2
+    INTEGER, DIMENSION (:), INTENT(IN) :: G1, G2
+    INTEGER, INTENT(INOUT) :: ma, mi
+    INTEGER :: i, c
+
+    ALLOCATE (L1(Length))
+    ALLOCATE (L1_1(Length))
+
+    ALLOCATE (L2(Length))
+    ALLOCATE (L2_1(Length))
+
+
+    ALLOCATE (VAL_1(Length))
+    ALLOCATE (COL_1(Length))
+
+    ALLOCATE (VAL_2(Length))
+    ALLOCATE (COL_2(Length))
+
+    ALLOCATE(row1(length))
+    ALLOCATE(row2(length))
+
+
+    mi = MIN(G1(1), G2(1))
+    ma = MAX(G1(1), G2(1))
+    DO i = 2 , N
+        mi = MIN(mi, G1(i))
+        ma = MAX(ma, G1(i))
+        mi = MIN(mi, G2(i))
+        ma = MAX(ma, G2(i))
+    ENDDO
+
+    DO i = 1, ma - mi + 1
+        L1(i) = 0
+        L1_1(i) = 0
+
+        L2(i) = 0
+        L2_1(i) = 0
+    ENDDO
+
+    DO i=1, Length
+        VAL_1(i) = 0
+        COL_1(i) = 0 
+        VAL_2(i) = 0
+        COL_2(i) = 0 
+    enddo
+
+    DO i = 1, N
+        L1(G1(i) - mi + 1) = L1(G1(i) - mi + 1) + 1
+        L2(G2(i) - mi + 1) = L2(G2(i) - mi + 1) + 1
+    ENDDO
+
+    DO i = 1, ma - mi + 1
+        L1_1(i) = L1(i)
+        L2_1(i) = L2(i)
+    ENDDO
+
+    c = 1
+    row1(1) = 1
+    do i = 1, ma - mi + 1
+        c = c + L1(i)
+        row1(i+1) = c
+    enddo
+
+    c = 1
+    row2(1) = 1
+    do i = 1, ma - mi + 1
+        c = c + L2(i)
+        row2(i+1) = c
+    enddo
+ENDSUBROUTINE
+
+
+SUBROUTINE LoadSpareMatrix(L1, L1_1, L2, L2_1, VAL_1, VAL_2, COL_1, COL_2, row1, row2, G1, G2, mi)
+    IMPLICIT NONE
+    INTEGER, DIMENSION (:), INTENT(IN) :: L1, L2
+    INTEGER, DIMENSION (:), INTENT(INOUT) :: VAL_1, VAL_2, COL_1, COL_2
+    INTEGER, DIMENSION (:), INTENT(INOUT) :: row1, row2
+    INTEGER, DIMENSION (:), INTENT(INOUT) :: L1_1, L2_1
+    INTEGER, DIMENSION (:), INTENT(IN) :: G1, G2
+    INTEGER, INTENT(IN) :: mi
+    INTEGER :: i, c, k, offset, j
+
+    do i = 1, N 
+        k = G1(i) - mi + 1
+        j = row1(k)
+        offset = L1(k) - L1_1(k)
+        VAL_1(j + offset) = i
+        COL_1(j + offset) = G2(i)
+        L1_1(k) = L1_1(k) - 1
+        
+        k = G2(i) - mi + 1
+        j = row2(k)
+        offset = L2(k) - L2_1(k)
+        VAL_2(j + offset) = i
+        COL_2(j + offset) = G1(i)
+        L2_1(k) = L2_1(k) - 1
+    enddo
+ENDSUBROUTINE    
+
 END MODULE
