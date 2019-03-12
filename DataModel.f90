53a06791a5626b94bfd906ad4d054b86736da673
tienthanh <tienthanh.nguyen@tum.de>
2019-03-12 10:41:40 -0400

    add new variables

--- DataModel.f90
+++ DataModel.f90
@@ -5,9 +5,11 @@ module DataModel
     
     
     
-    INTEGER :: N = 1140404
-    INTEGER :: M = 1140407
-    INTEGER :: Length = 12 * 100000
+    !INTEGER :: N = 1140404
+    !INTEGER :: M = 1140407
+    INTEGER :: N = 285935
+    INTEGER :: M = 285950
+    INTEGER :: Length = 29 * 10000
 
     integer :: N_Length = 800
 
@@ -15,14 +17,17 @@ module DataModel
     !INTEGER :: M = 1007
     !INTEGER :: Length = 12 * 100
 
-    INTEGER :: trial = 1000*1000
+    INTEGER :: trial = 1000*10  !00
 
-    INTEGER :: N_Loop = 800
+    INTEGER :: N_Loops = 285935
 
     !INTEGER :: c = 0
     !Length = 300*1000
     
 
     integer :: chunk_size
+
+    CHARACTER(100) :: num1char
+    INTEGER :: num_of_threads
     contains
 end module DataModel
