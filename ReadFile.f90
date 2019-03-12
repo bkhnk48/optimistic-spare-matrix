cdbca30eabc3cd9ca93c2dd49e60c931373aebbf
tienthanh <tienthanh.nguyen@tum.de>
2019-03-12 10:46:01 -0400

    use DataModel and inside each subroutine, take N as number of lines to read

--- ReadFile.f90
+++ ReadFile.f90
@@ -1,22 +1,22 @@
 MODULE ReadFile
+    USE DataModel
 contains
 
 SUBROUTINE readREAL(ios, fh, X, fn)
     IMPLICIT NONE
     CHARACTER (len=*), INTENT(IN) :: fn !file name
-    !CHARACTER (len=*), INTENT(IN) :: tp !type data 
     INTEGER, INTENT(INOUT) :: ios  
     INTEGER, INTENT(IN) :: fh
-    INTEGER :: i  !, temp
+    INTEGER :: i  
     DOUBLE PRECISION :: t
+    
     DOUBLE PRECISION, DIMENSION(:), INTENT(INOUT) :: X
 
     i = 0
     OPEN(fh, file=fn, iostat=ios, action='READ')
 
     IF (ios /= 0) STOP 1
-    DO
-        i = i + 1
+    DO i = 1, N
         READ(fh, *, iostat=ios) t
         IF (ios /= 0) exit
         X(i) = t
@@ -29,6 +29,7 @@ SUBROUTINE dummy(X, XA1, XA2, Y)
     IMPLICIT NONE
     DOUBLE PRECISION, DIMENSION(:), INTENT(INOUT) :: X, XA1, XA2, Y
 
+    X = X + X
     print *, X
 ENDSUBROUTINE
 
@@ -38,7 +39,7 @@ SUBROUTINE readINT(ios, fh, X, fn)
     
     INTEGER, INTENT(INOUT) :: ios  
     INTEGER, INTENT(IN) :: fh
-    INTEGER :: i  !, temp
+    INTEGER :: i  
     INTEGER :: t
     INTEGER, DIMENSION(:), INTENT(INOUT) :: X
 
@@ -46,9 +47,7 @@ SUBROUTINE readINT(ios, fh, X, fn)
     OPEN(fh, file=fn, iostat=ios, action='READ')
 
     IF (ios /= 0) STOP 1
-    DO
-        i = i + 1
-        
+    DO i = 1, N
         READ(fh, *, iostat=ios) t
         IF (ios /= 0) exit
         X(i) = t
