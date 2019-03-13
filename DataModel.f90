e73c20a388766905114e130a24637d58709b5d25
tienthanh <tienthanh.nguyen@tum.de>
2019-03-13 11:55:52 -0400

    change values of variable in DataModel.f90

--- DataModel.f90
+++ DataModel.f90
@@ -9,7 +9,8 @@ module DataModel
     !INTEGER :: M = 1140407
     INTEGER :: N = 285935
     INTEGER :: M = 285950
-    INTEGER :: Length = 29 * 10000
+    INTEGER :: Length  = 29 * 10  * 1000
+    !INTEGER :: Length = 12 * 100 * 1000
 
     integer :: N_Length = 800
 
@@ -17,7 +18,7 @@ module DataModel
     !INTEGER :: M = 1007
     !INTEGER :: Length = 12 * 100
 
-    INTEGER :: trial = 1000*10  !00
+    INTEGER :: trial = 1000!*10  !00
 
     INTEGER :: N_Loops = 285935
 
@@ -27,7 +28,14 @@ module DataModel
 
     integer :: chunk_size
 
+    INTEGER :: mi, ma
     CHARACTER(100) :: num1char
     INTEGER :: num_of_threads
+
+    !Enumeration types
+    INTEGER :: SMALL_SIZE = 29 * 10000 ! for 285935
+    INTEGER :: BIG_SIZE = 12 * 100 * 1000! for 1140404
+    
     contains
+
 end module DataModel
