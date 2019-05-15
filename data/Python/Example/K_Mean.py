9641066161010cd86c3fa75e0b4dcee546eb8e45
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-05-15 12:58:27 -0400

    add new comments to explain plot as well as test __main__

--- data/Python/Example/K_Mean.py
+++ data/Python/Example/K_Mean.py
@@ -4,31 +4,36 @@ import matplotlib.pyplot as plt
 from scipy.spatial.distance import cdist
 np.random.seed(11)
 
-
+if __name__ == "__main__": #cau lenh kiem tra dieu kien IF
+    print('FFFFFFFF')
 means = [[2, 2], [8, 3], [3, 6]]
 cov = [[1, 0], [0, 1]]
 N = 500
 X0 = np.random.multivariate_normal(means[0], cov, N)
-X1 = np.random.multivariate_normal(means[1], cov, N)
-X2 = np.random.multivariate_normal(means[2], cov, N)
+#X1 = np.random.multivariate_normal(means[1], cov, N)
+#X2 = np.random.multivariate_normal(means[2], cov, N)
 
-X = np.concatenate((X0, X1, X2), axis = 0)
-K = 3
-
-original_label = np.asarray([0]*N + [1]*N + [2]*N).T
+#X = np.concatenate((X0, X1, X2), axis = 0)
+#K = 3
+#print('X0 = ', X0)
+#original_label = np.asarray([0]*N + [1]*N + [2]*N).T
+original_label = np.asarray([0]*N ).T
+#do chi hien thi X0 nen original_label chi nhan [0]*N
 def kmeans_display(X, label):
     K = np.amax(label) + 1
     X0 = X[label == 0, :] #truy cap hang 0 cua ma tran X
-    X1 = X[label == 1, :] #truy cap hang 1 cua ma tran X
-    X2 = X[label == 2, :] #truy cap hang 2 cua ma tran X
+    #X1 = X[label == 1, :] #truy cap hang 1 cua ma tran X
+    #X2 = X[label == 2, :] #truy cap hang 2 cua ma tran X
     
     plt.plot(X0[:, 0], X0[:, 1], 'b^', markersize = 4, alpha = .8)
     #truy cap cot 0 cua ma tran X0, sau do la cot 1 cua X0 
-    plt.plot(X1[:, 0], X1[:, 1], 'go', markersize = 4, alpha = .8)
-    plt.plot(X2[:, 0], X2[:, 1], 'rs', markersize = 4, alpha = .8)
+    #ham plot nhan dau vao la danh sach cac toa do 
+    #theo truc X, tiep theo la danh sach cac toa do theo truc Y
+    #plt.plot(X1[:, 0], X1[:, 1], 'go', markersize = 4, alpha = .8)
+    #plt.plot(X2[:, 0], X2[:, 1], 'rs', markersize = 4, alpha = .8)
 
     plt.axis('equal')
     plt.plot()
     plt.show()
     
-kmeans_display(X, original_label)
+kmeans_display(X0, original_label)
