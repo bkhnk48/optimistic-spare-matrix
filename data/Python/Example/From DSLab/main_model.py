6e039737c559070704431a27fcfb796a7cd47f0f
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-05-15 23:06:42 -0400

    add new import statement

---
+++ data/Python/Example/From DSLab/main_model.py
@@ -0,0 +1,4 @@
+from __future__ import division, print_function, unicode_literals
+import numpy as np 
+from normalized_data import normalize_and_add_one
+from RidgeRegression import RidgeRegression
