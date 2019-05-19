056476f39450271e84b3e35e8bc13bc627a3f958
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-05-19 13:58:56 +0700

    first attempt to pass data in lines into array

--- data/Python/Example/From DSLab/main_model.py
+++ data/Python/Example/From DSLab/main_model.py
@@ -11,15 +11,33 @@ def get_data(relative_path):
     
     mypath = Path().absolute()
     path = str(mypath) + relative_path
-    print(path)
+    #print(path)
+    #X = np.array([[]]) #np.array([])
     
+    Y = []
     file = open(path, "r")
-    profile_list = file.readlines()
-    print(len(profile_list))
-    print(profile_list[72])
+    list = file.readlines()
+    #print(len(profile_list))
+    #print(profile_list[72])
+    #row_ids = np.array(range(len(list)))
+    list = list[72:]
+    #X = [[] for i in range(len(list))]
+    X = np.arange(len(list))
+    Y = [[] for i in range(len(list))]
+    #print(list[0])
+    for i in range(len(list)):
+        temp = list[i].split()
+        temp1 = temp[1: len(temp) - 2]
+        temp2 = temp[len(temp) - 1]
+        #X[i] = X.append(np.array(temp1))
+        X[i].append(temp1)
+        #Y = Y.append(temp2)
+        Y[i].append(temp2)
     file.close()
-    return 'X', 'Y'
+    return X, Y
 if __name__ == '__main__':
     print("Main model")
-    #X, Y = get_data(path = '/datasets/death_rates_data.txt')
+    
     X, Y = get_data(relative_path = '/datasets/death_rates_data.txt')
+    #X = normalize_and_add_one(X)
+    print(X)
