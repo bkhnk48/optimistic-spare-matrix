f82f8210b8b44c6a268e5131de32b369671b84cf
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-08-26 18:52:15 +0700

    Add new data structures represent switches and hosts

--- program C/serial PoLIN/DataStructures.c
+++ program C/serial PoLIN/DataStructures.c
@@ -5,30 +5,48 @@
 #include "ErrorReport.c"
   
 // A structure to represent a queue 
-struct Queue 
+struct PACKET 
 { 
     int src;
     int dst;
     int nHop;
     int id; 
-    struct Queue *next; 
+    struct PACKET *next; 
 }; 
 
+typedef struct PACKET *Packet; //Define packet as pointer of data type struct Queue
 
-typedef struct Queue *Packet; //Define packet as pointer of data type struct Queue
+struct HOST{
+    int aSwitch;
+    int outPort;
+    int lastID; //ID cua packet cuoi cung da tao ra
+    Packet *queue;//Source queue
+    Packet *last;//phan tu cuoi cung trong queue
+};
 
 
-struct PortOfSwitch
+
+typedef struct HOST *host;
+
+
+struct PairPortsOfSwitch
 {
     Packet inputPort[5];
     Packet outputPort[5];
 };
 
-typedef struct PortOfSwitch *IntegratedPort; 
+typedef struct PairPortsOfSwitch *IntegratedPort; 
+
+
+struct SWITCH{
+    int host;//-1 nghia la khong ket noi voi host nao ca
+    IntegratedPort *integratedPorts;
+};
+
 
 Packet createPacket(int id, int src, int dst, int nHop){
     Packet temp; // declare a node
-    temp = (Packet)malloc(sizeof(struct Queue)); // allocate memory using malloc()
+    temp = (Packet)malloc(sizeof(struct PACKET)); // allocate memory using malloc()
     temp->id = id;
     temp->src = src;
     temp->dst = dst;
