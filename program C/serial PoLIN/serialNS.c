4bb1a18d46b95990ecade67e1671fd153c7f4015
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-08-26 17:29:01 +0700

    serialNS.c now uses new arrays for packets in hosts as well as new function to initialize packets

--- program C/serial PoLIN/serialNS.c
+++ program C/serial PoLIN/serialNS.c
@@ -9,7 +9,7 @@ int main(int argc, char** argv)
     int numOfSwitches = 20;
     int numOfHosts = 16;
     int numOfInforAboutSwitch = 12;
-    //int switches[numOfSwitches][numOfPorts];
+
     int **AdjOfSwitches = NULL;//Mang luu tru cac Adjancent cua SWiTCHES
     IntegratedPort **IntegratedPortOfSwitches = NULL;//Mang luu tru cac Integrated Port cua SWITCHES
     AdjOfSwitches = malloc( sizeof * AdjOfSwitches * numOfSwitches );
@@ -26,21 +26,39 @@ int main(int argc, char** argv)
       AdjOfSwitches[i] = malloc( sizeof *AdjOfSwitches[i] * numOfPorts );
     }
 
-    echoIntegratedPorts(IntegratedPortOfSwitches, numOfPorts, numOfSwitches);
-    //int buffers[numOfSwitches * numOfPorts][numOfInforAboutSwitch];
-    Packet hosts[numOfHosts];
+    Packet *hosts = NULL; //Source Queue cua cac Hosts
+    hosts = malloc(numOfHosts * sizeof(Packet));
+    
+    Packet *lasts = NULL;//Luu tru phan tu cuoi cung trong Source Queue cua cac Hosts
+    lasts = malloc(numOfHosts * sizeof(Packet));
 
+    int *lastIDs = NULL;
+    lastIDs = calloc(numOfHosts, sizeof(int));
+    
     assignAdj(AdjOfSwitches, 10, 11);
 
-    for(int i = 0; i < numOfHosts; i++)
+    /*for(int i = 0; i < numOfHosts; i++)
     {   
         if(i == 32 - numOfSwitches)
             hosts[i] = createPacket(i + numOfSwitches, i + numOfSwitches, 21, 0);
         else
             hosts[i] = //i + numOfSwitches;  
                     createPacket(i + numOfSwitches, -1, -1, 0);
-    }
+    }*/
 
+    printf("Here\n");
+
+
+    printf("\nLAST ID %d", lastIDs[32 - numOfSwitches]);
+    generatePacket(hosts, lasts, 32, 21, numOfSwitches, lastIDs);
+
+    display(hosts, numOfHosts);
+
+    printf("\n%d", lastIDs[32 - numOfSwitches]);
+    printf("\n====================\n");
+    generatePacket(hosts, lasts, 32, 21, numOfSwitches, lastIDs);
+    display(hosts, numOfHosts);
+    printf("\n%d", lastIDs[32 - numOfSwitches]);
     //display(hosts, numOfHosts);
 
     //echo(AdjOfSwitches, numOfPorts, numOfSwitches);
