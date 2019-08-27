c481cb252df82f5e7ea04aadc2f434150e18a46b
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-08-28 05:39:19 +0700

    Reverse AdjOfSwitches temporarily

--- program C/serial PoLIN/serialNS.c
+++ program C/serial PoLIN/serialNS.c
@@ -10,12 +10,17 @@ int main(int argc, char** argv)
     int numOfHosts = 16;
     int numOfInforAboutSwitch = 12;
 
-    //int **AdjOfSwitches = NULL;//Mang luu tru cac Adjancent cua SWiTCHES
-    
+    int **AdjOfSwitches = NULL;//Mang luu tru cac Adjancent cua SWiTCHES, tam thoi de lai bien nay
+    //vi chua tao ra duoc ham doc file
+    Switch *switches = NULL;//Mang luu tru cac switches cua network
+    Host *hosts = NULL;
+
     IntegratedPort **IntegratedPortOfSwitches = NULL;//Mang luu tru cac Integrated Port cua SWITCHES
-    //AdjOfSwitches = malloc( sizeof * AdjOfSwitches * numOfSwitches );
+    AdjOfSwitches = malloc( sizeof * AdjOfSwitches * numOfSwitches );
 
     IntegratedPortOfSwitches = malloc( sizeof * IntegratedPortOfSwitches * numOfSwitches );
+    switches = malloc(sizeof * switches * numOfSwitches);
+    hosts = malloc(sizeof * hosts * numOfHosts);
 
     for (int i = 0; i < numOfSwitches; i++ )
     {
@@ -24,11 +29,15 @@ int main(int argc, char** argv)
 
     for (int i = 0; i < numOfSwitches; i++ )
     {
-      //AdjOfSwitches[i] = malloc( sizeof *AdjOfSwitches[i] * numOfPorts );
+      IntegratedPort *integratedPorts = malloc(sizeof * integratedPorts * numOfPorts);
+      Switch aSwitch = malloc(sizeof aSwitch);
+      switches[i] = aSwitch;
+      switches[i] -> integratedPorts = integratedPorts;
+      AdjOfSwitches[i] = malloc( sizeof *AdjOfSwitches[i] * numOfPorts );
     }
 
-    Packet *hosts = NULL; //Source Queue cua cac Hosts
-    hosts = malloc(numOfHosts * sizeof(Packet));
+    //Packet *hosts = NULL; //Source Queue cua cac Hosts
+    //hosts = malloc(numOfHosts * sizeof(Packet));
     
     Packet *lasts = NULL;//Luu tru phan tu cuoi cung trong Source Queue cua cac Hosts
     lasts = malloc(numOfHosts * sizeof(Packet));
@@ -36,30 +45,22 @@ int main(int argc, char** argv)
     int *lastIDs = NULL;
     lastIDs = calloc(numOfHosts, sizeof(int));
     
-    //assignAdj(AdjOfSwitches, 10, 11);
+    assignAdj(AdjOfSwitches, 10, 11);
 
-    InitIntegratedPorts(IntegratedPortOfSwitches, numOfPorts, numOfSwitches);
+    //InitIntegratedPorts(IntegratedPortOfSwitches, numOfPorts, numOfSwitches);
 
 
-    printf("\nLAST ID %d", lastIDs[32 - numOfSwitches]);
-    insertToSourceQueue(hosts, lasts, 32, 21, numOfSwitches, lastIDs);
+    //printf("\nLAST ID %d", lastIDs[32 - numOfSwitches]);
+    //insertToSourceQueue(hosts, lasts, 32, 21, numOfSwitches, lastIDs);
 
-    display(hosts, numOfHosts);
-
-    printf("\n%d", lastIDs[32 - numOfSwitches]);
-    printf("\n====================\n");
-    insertToSourceQueue(hosts, lasts, 32, 21, numOfSwitches, lastIDs);
-    display(hosts, numOfHosts);
-    printf("\n%d", lastIDs[32 - numOfSwitches]);
-
-    //int *port = getNumOfPort(32, AdjOfSwitches, numOfPorts, numOfSwitches);
-    //printf("Switch: %d Port: %d", port[0], port[1]);
     //display(hosts, numOfHosts);
 
-    //echo(AdjOfSwitches, numOfPorts, numOfSwitches);
-    //printf("%d", switches[0][1]);
+    //printf("\n%d", lastIDs[32 - numOfSwitches]);
+    printf("\n====================\n");
+    //insertToSourceQueue(hosts, lasts, 32, 21, numOfSwitches, lastIDs);
+    //display(hosts, numOfHosts);
+    //printf("\n%d", lastIDs[32 - numOfSwitches]);
 
-    //printf("\nHello World\n");
     return 0;
 }
 
