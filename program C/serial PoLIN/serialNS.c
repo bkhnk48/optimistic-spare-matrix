d0eb7022758fefeee93465e580c00dfd097aea8d
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-08-28 21:14:09 +0700

    Remove redundant comments

--- program C/serial PoLIN/serialNS.c
+++ program C/serial PoLIN/serialNS.c
@@ -33,6 +33,7 @@ int main(int argc, char** argv)
     {
       Host tempHost = malloc(sizeof tempHost);
       hosts[i] = tempHost;
+      hosts[i]->lastID = -1;
     }
 
     for (int i = 0; i < numOfSwitches; i++ )
@@ -45,33 +46,18 @@ int main(int argc, char** argv)
       AdjOfSwitches[i] = malloc( sizeof *AdjOfSwitches[i] * numOfPorts );
     }
 
-    //Packet *hosts = NULL; //Source Queue cua cac Hosts
-    //hosts = malloc(numOfHosts * sizeof(Packet));
-    
-    //Packet *lasts = NULL;//Luu tru phan tu cuoi cung trong Source Queue cua cac Hosts
-    //lasts = malloc(numOfHosts * sizeof(Packet));
-
-    //int *lastIDs = NULL;
-    //lastIDs = calloc(numOfHosts, sizeof(int));
-
     assignAdj(AdjOfSwitches, 10, 11);
     assignAdjant(switches, hosts, AdjOfSwitches, numOfSwitches, numOfPorts);
 
     //InitIntegratedPorts(IntegratedPortOfSwitches, numOfPorts, numOfSwitches);
 
-
-    //printf("\nLAST ID %d", lastIDs[32 - numOfSwitches]);
-    //insertToSourceQueue(hosts, lasts, 32, 21, numOfSwitches, lastIDs);
-
     insertSourceQueue(hosts, 32, 21, numOfSwitches);
 
-    //display(hosts, numOfHosts);
+    display(hosts, numOfHosts);
 
-    //printf("\n%d", lastIDs[32 - numOfSwitches]);
     printf("\n====================\n");
     insertSourceQueue(hosts, 32, 21, numOfSwitches);
-    //display(hosts, numOfHosts);
-    //printf("\n%d", lastIDs[32 - numOfSwitches]);
+    display(hosts, numOfHosts);
 
     return 0;
 }
