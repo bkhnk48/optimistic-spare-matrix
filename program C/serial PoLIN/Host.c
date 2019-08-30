cdcf2acd40ededdc1e371ac88fa103d2a937c3ff
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-08-30 09:12:01 +0700

    Add new functions: (i) dequeue, (ii)getOutPort. Include the library Host.h

--- program C/serial PoLIN/Host.c
+++ program C/serial PoLIN/Host.c
@@ -1,28 +1,30 @@
-#include<stdio.h>
-#include "DataStructures.c"
+#include "Host.h"
 
 int CanReceive(Host host, Switch *switches)
 {
+    IntegratedPort ports = getOutPort(host, switches);
+    return ports->bufferIn;
+}
+
+IntegratedPort getOutPort(Host host, Switch *switches)
+{
     int switchID = host->aSwitch;
     Switch theSwitch = switches[switchID];
-    int i = 0;
-    int count = 0;
     int t = host->outPort;
     IntegratedPort ports = (theSwitch->integratedPorts[t]);
-    //Packet outputPort[5] ;
-    //outputPort = ports->outputPort;
-    if(ports->outputPort == NULL)
+    return ports;
+}
+
+//Packet 
+int dequeue(Host host, Packet* first)
+{
+    if(host -> queue != NULL)
     {
-        return 0;
+        *first = (host->queue);
+        host->queue = (host->queue)->next;
+        //return first;
+        return 1;
     }
-    for(i = 0; i < 5; i++)
-    {
-        if(ports->outputPort[i] == NULL)
-            count++;
-    }
-    switch (count)
-    {
-        case 0: return 0;       break;
-        default:    return 1;   break;
-    }
+    //return NULL;
+    return 0;
 }
