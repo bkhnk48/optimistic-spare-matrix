0e50dbdadd43b993b4b76fd36f12afb3bb858796
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-09-03 15:46:37 +0700

    change prototype of addPacketToBuffer and move function: getEmptySlot

--- program C/serial PoLIN/IntegratedPort.c
+++ program C/serial PoLIN/IntegratedPort.c
@@ -1,30 +1,9 @@
 #include "IntegratedPort.h"
 
-void addPacketToBuffer(Packet p, IntegratedPort ports)
+void addPacketToBuffer(Packet p, Host host)
 {
-    int index = getEmptySlot(ports);
-    ports->inputPort[index] = p;
+    IntegratedPort outPort = host->outPort;
+    int index = getEmptySlot(outPort->outputPort);
+    outPort -> outputPort[index] = p;
 }
 
-int getEmptySlot(IntegratedPort ports)
-{
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wpointer-to-int-cast"
-    unsigned int slot4 = (unsigned int)(ports->inputPort[4]);
-    unsigned int slot3 = (unsigned int)(ports->inputPort[3]);
-    unsigned int slot2 = (unsigned int)(ports->inputPort[2]);
-    unsigned int slot1 = (unsigned int)(ports->inputPort[1]);
-    unsigned int slot0 = (unsigned int)(ports->inputPort[0]);
-    #pragma GCC diagnostic pop
-    switch (slot0)
-    {   case 0: return 0;  break;     }
-    switch (slot1)
-    {   case 0: return 1;  break;     }
-    switch (slot2)
-    {   case 0: return 2;  break;     }
-    switch (slot3)
-    {   case 0: return 3;  break;     }
-    switch (slot4)
-    {   case 0: return 4;  break;     }
-    return -1;
-}
