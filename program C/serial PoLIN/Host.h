f1331fe0dcac86a9171aa97e228919834033a266
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-09-03 15:38:31 +0700

    Remove function: CanReceive of Host.h

--- program C/serial PoLIN/Host.h
+++ program C/serial PoLIN/Host.h
@@ -1,10 +1,15 @@
 #include<stdio.h>
 #include "DataStructures.c"
 
-int CanReceive(Host host, Switch *switches);
+//int CanReceive(Host host, Switch *switches);
 
 IntegratedPort getOutPort(Host host, Switch *switches);
 
 //Packet 
 int dequeue(Host host, Packet *first);
 //int dequeue(Host host, Packet** first);
+
+int getEmptySlot(Packet ports[]);
+
+void forwardToLink(Host self);
+
