721827fde2b0c6ac71e8f6f9b8a21db326f752cc
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-11-08 16:28:23 +0700

    Add serialNS.c

---
+++ program C/SSiNET/serialNS.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "Init.c"
+
+
+
+int main(int argc, char** argv) 
+{
+    int numOfPorts = 4;
+    int numOfSwitches = 20;
+    int numOfHosts = 16;
+    int numOfInforAboutSwitch = 12;
+    int bandwidth = 1000*1000;
+
+    //De luu thong tin ve link va cac goi du lieu truyen tren link
+    //cung nhu cac su kien xay ra tren link
+    //ta dung mang 1 chieu nhu sau:
+    //  tren mot chieu se co id cua nut nguon, id cua nut dich, id cua goi tin, id cua Host nguon,
+    //  id cua Host dich, nix-vector (?), kich thuoc goi tin, bit dau tien cua goi tin se den vao luc nao,
+    //  toan bo goi tin se den nut dich khi nao?, bang thong
+    int countOfInfoInLink = 10;
+    int numOfLinks = numOfSwitches * numOfPorts + numOfHosts;
+    int *Link = NULL;
+    Link = malloc(sizeof * Link * (numOfLinks * countOfInfoInLink));
+
+    assignAdj(Link, numOfPorts, numOfSwitches, bandwidth);
+
+    echo(Link, numOfLinks );
+    return 0;
+}
+
