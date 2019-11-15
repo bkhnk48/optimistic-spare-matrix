782f1b56b122874fc5dcaf4c9165f7f124d59204
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-11-15 11:15:36 +0700

    New function to assign Switch and show its packets

--- program C/SSiNET/Switch.c
+++ program C/SSiNET/Switch.c
@@ -10,6 +10,28 @@ void showSwitchIDs(int *SwitchIDs, int numOfSwitches)
     }
 }
 
+void showSwitchPacketsAtPort(int **SwitchPortPID, 
+                int **SwitchPortSrcIDs, int **SwitchPortDstIDs,
+                int bufferSize, int numOfSwitches)
+{
+    int i = 0, j = 0;
+    for(i = 0; i < numOfSwitches; i++)
+    {
+        for(j = 0; j < bufferSize; j++)
+        {
+            if(SwitchPortPID[i][j] != -1)
+            {
+                printf("\tWe have packet id = %d stays in port of switch: %d. ", 
+                        SwitchPortPID[i][j],
+                        i 
+                    );
+                printf("It is moving from %d to %d", SwitchPortSrcIDs[i][j], 
+                            SwitchPortDstIDs[i][j]);
+            }
+        }
+    }
+}
+
 void assignSwitchIDs(int *SwitchIDs, int numOfSwitches)
 {
     int i = 0, j = 0;
@@ -21,16 +43,19 @@ void assignSwitchIDs(int *SwitchIDs, int numOfSwitches)
 }
 
 
-/*void assignSwitchPackets(int **SwitchInportIDPcks, 
-                            int **SwitchOutportIDPcks, int bufferSize, int numOfSwitches)
+void assignSwitchPackets(int **SwitchInportPID, 
+                int **SwitchInportSrcIDs,
+                int **SwitchInportDstIDs,
+                int bufferSize, int numOfSwitches)
 {
     int i = 0, j = 0;
     for(i = 0; i < numOfSwitches; i++)
     {
         for(j = 0; j < bufferSize; j++)
         {
-            SwitchInportIDPcks[i][j] = -1;
-            SwitchOutportIDPcks[i][j] = -1;
+            SwitchInportPID[i][j] = -1;
+            SwitchInportSrcIDs[i][j] = -1;
+            SwitchInportDstIDs[i][j] = -1;
         }
     } 
-}*/
+}
