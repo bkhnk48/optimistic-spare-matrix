d1a321ca83bde499aa16e68d11a531bc4dffed12
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-11-15 15:56:38 +0700

    new function to set addresses of nodes

--- program C/SSiNET/Init.c
+++ program C/SSiNET/Init.c
@@ -5,8 +5,9 @@
 
 int assignLink(int *Link, int width, int height, int bandwidth);
 
-
 void echo(int *Link, int count);
+
+void setAddresses(int **Addresses, int numOfPorts);
 
 void echo(int *Link, int count)
 {
@@ -100,4 +101,46 @@ int assignLink(int *Link, int width, int height, int bandwidth)
     }
     return count; //Could it return 48?
 
+}
+
+void setAddresses(int **Addresses, int numOfPorts)
+{
+    int i = 0, j = 0, p = 0, s = 0, e, h;
+    int numOfSwitches = 5 * numOfPorts * numOfPorts / 4;
+    int numEachPod = numOfPorts * numOfPorts / 4 + numOfPorts;
+    int numServers = numOfPorts * numOfPorts * numOfPorts / 4;
+    int numPodSwitches = numOfPorts * numOfPorts; int offset = 0;
+
+    // IDs for pod's switches
+    for (p = 0; p < numOfPorts; p++) {
+        offset = numEachPod * p;
+        for (int s = 0; s < numOfPorts; s++) {
+            int switchId = offset + numOfPorts * numOfPorts / 4 + s;
+            Addresses[switchId][0] = 10;   Addresses[switchId][1] = p;
+            Addresses[switchId][2] = s;    Addresses[switchId][3] = 1;
+            printf("Address of pod switch: %d is %d.%d.%d.%d\n", switchId, 10, p, s, 1);
+        }
+    }
+    offset = numPodSwitches + numServers;
+    // IDs for core switches
+    for (j = 1; j <= numOfPorts / 2; j++) {
+        for (i = 1; i <= numOfPorts / 2; i++) {
+            int switchId = offset + (j - 1) * numOfPorts / 2 + i - 1;
+            Addresses[switchId][0] = 10;       Addresses[switchId][1] = numOfPorts;
+            Addresses[switchId][2] = j;        Addresses[switchId][3] = i;
+            printf("Address of core switch: %d is %d.%d.%d.%d\n", switchId, 10, numOfPorts, j, i);
+        }
+    }
+    offset = numEachPod * p;
+    for (p = 0; p < numOfPorts; p++) {
+        offset = numEachPod * p;
+        for (e = 0; e < numOfPorts / 2; e++) {
+            for (h = 2; h <= numOfPorts / 2 + 1; h++) {
+                int serverId = offset + e * numOfPorts / 2 + h - 2;
+                Addresses[serverId][0] = 10;   Addresses[serverId][1] = p;
+                Addresses[serverId][2] = e;    Addresses[serverId][3] = h;
+                printf("Address of server: %d is %d.%d.%d.%d\n", serverId, 10, p, e, h);
+            }
+        }
+    }
 }
