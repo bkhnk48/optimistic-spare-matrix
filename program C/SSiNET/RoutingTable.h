bc1ba8295154d242293d46e705f56dc747b2742a
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-11-24 09:20:20 +0700

    Add new prototypes for some functions

--- program C/SSiNET/RoutingTable.h
+++ program C/SSiNET/RoutingTable.h
@@ -9,3 +9,7 @@ int setHavingCorePrefix(int **HavingCorePrefix, int numOfPorts);
 void setSuffix(int **Suffix, int numOfPorts);
 void setPrefix(int **Prefix, int numOfPorts);
 void setCorePrefix(int **CorePrefix, int numOfPorts);
+
+void showTwoLevelsRoutingTable(int **HavingSuffix, int **HavingPrefix, int **HavingCorePrefix
+                        , int **Suffix, int **Prefix, int **CorePrefix, int numOfPorts
+                            );
