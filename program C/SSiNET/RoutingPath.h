73e715b84a303763c1a1444814c082373a958911
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-11-26 04:53:56 +0700

    move prototypes of functions

--- program C/SSiNET/RoutingPath.h
+++ program C/SSiNET/RoutingPath.h
@@ -1,10 +1,5 @@
 #include "RoutingPath.c"
 
 //int * getNixVector(int src,int dst, RAlgorithm ra, Graph g, int* hopCount);
-void getNixVector(int src,int dst, RAlgorithm ra, Graph g
-                    //, int* hopCount
-                    );
 
-int getNextNode(int src, int curr, int dst, RAlgorithm ra, Graph g//, int* hopCount
-                    );
 
