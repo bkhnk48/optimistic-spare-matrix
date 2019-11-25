56b0e373075aac4a9da9b136715fd350ce3df6ad
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-11-26 04:54:32 +0700

    add new prototypes to avoid [-Wimplicit-function-declaration]

--- program C/SSiNET/RoutingPath.c
+++ program C/SSiNET/RoutingPath.c
@@ -2,6 +2,13 @@
 #include "RoutingTable.h"
 
 void getNixVector(int src,int dst, RAlgorithm ra, Graph g
+                    //, int* hopCount
+                    );
+
+int getNextNode(int src, int curr, int dst, RAlgorithm ra, Graph g//, int* hopCount
+                    );
+                    
+void getNixVector(int src,int dst, RAlgorithm ra, Graph g
                         //, int* hopCount
                         )
 {
