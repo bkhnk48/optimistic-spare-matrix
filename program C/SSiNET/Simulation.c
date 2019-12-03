d0f8a3315efc82d87d7fcb4aefd71b9029c92878
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-12-03 17:51:56 +0700

    new implement for generating packets

--- program C/SSiNET/Simulation.c
+++ program C/SSiNET/Simulation.c
@@ -13,7 +13,7 @@ void run(Graph g, RAlgorithm ra, int *path, int stop, int curr)
         int linkID: ID of link from host itself
         int hostID: id cua host (trong danh sach cac nut cua toan mang)
 
-        int **allEvents;
+        int *allEvents;
                     //allEvents[0]: credit count
                     //allEvents[1]: id of the sent packet
                     //allEvents[2]: id of received packet in next switch
@@ -27,8 +27,8 @@ void run(Graph g, RAlgorithm ra, int *path, int stop, int curr)
     */
 
     int i, j;
-    //Host Hosts = g->Hosts;
-    int **Hosts;
+    Host *Hosts = g->Hosts;
+    //int **Hosts;
     int **Links = g->Links;
     int *SwitchIndexes = g->SwitchIndexes;
     int numOfHosts = g->numOfHosts;
@@ -36,103 +36,201 @@ void run(Graph g, RAlgorithm ra, int *path, int stop, int curr)
     int CYCLE_PACKET = g->CYCLE_PACKET;
     int dst = path[path[0] - 1];
     int RETRY_TIME = 3;
-    //while(curr < stop)
+    Queue **queues = g->queues;
+    
+    for(i = 0; i < numOfHosts; i++)
     {
-        for(i = 0; i < numOfHosts; i++)
+        int hostID = Hosts[i]->hostID;
+        int idOfLink = Hosts[i]->linkID;
+        int *allEvents = Hosts[i]->allEvents;
+        int credit = allEvents[0];
+        int sentID = allEvents[1];
+        int receivedID = allEvents[2];
+        int timeOfI = allEvents[3];
+        int timeOfB = allEvents[4];
+        int timeOfC = allEvents[5];
+        Queue *q = queues[i];
+        int **outport = Hosts[i]->outport;
+
+        //execute event A:
+        if(curr % CYCLE_PACKET == 0)//if this is the time to generate packet
         {
-            //create event A:
-            if(curr % CYCLE_PACKET == 0)
+            if(q[0]->id == -1)//if source queue KHONG con goi tin
             {
-                if(Hosts[i][1] == -1)//if source queue KHONG con goi tin
-                {
-                    Hosts[i][1] = curr / CYCLE_PACKET;//curr/CYCLE la id cua goi tin
-                    Hosts[i][11] = curr;//create event B
-                }
-                else
-                {
-                    Hosts[i][2] = curr / CYCLE_PACKET; //execute event A
-                }   
+                q[0]->id = curr / CYCLE_PACKET;//curr/CYCLE la id cua goi tin
+                q[0]->hostID = hostID;
+                q[0]->dstID = dst;
+                q[0]->next = NULL;
+                timeOfB = curr;//create event B
             }
-            int credit = Hosts[i][0];
-            //execute event C
-            int idOfLink = Hosts[i][12];
-            if( Links[idOfLink][2] != -1)//KHONG co goi tin tren duong truyen
+            else
             {
-                //Link[index][0]: id cua nut nguon
-                //Link[index][1]: id cua nut dich
-                //Link[index][2]: id cua goi tin
-                //Link[index][3] = -1;/*id cua host nguon*/
-                //Link[index][4] = -1;/*id cua host dich*/
-                //Link[index][5] = 0;/*nix-vector*/
-                //Link[index][6] = 0;/*kich thuoc cua goi tin*/
-                //Link[index][7] = 0;/*thoi diem bit dau den*/
-                //Link[index][8] = 0;/*thoi diem bit cuoi den*/
-                //Link[index][9] = 0;/*bandwidth*/
-                //Link[index][10] = 0;/*bang 0 nghia la nut nguon ko phai la Host*/
-                //Link[index][11] = 0;/*bang 1 nghia la nut dich la Host*/
-                //Link[index][12] = 0;//Hop count
 
-                if(Hosts[i][3] != -1//Neu co goi tin o top cua outport
-                    && Hosts[i][10] == curr //va goi tin o vi tri top nay co nhu cau duoc gui di
-                    && Hosts[i][3] != -1
-                    )
+                if(q[1]->id == -1)
                 {
-                    if(credit < BUFFER_SIZE)//neu inport cua switch tiep theo con cho trong
-                    {
-                        //tao su kien tren link
-                        Links[idOfLink][2] = Hosts[i][3];//gan id cua goi tin vao Link
-                        Links[idOfLink][3] = Hosts[i][14];//id cua host nguon
-                        Links[idOfLink][4] = dst;//id cua host dich
-                        Links[idOfLink][5] = path[1];//nix vector
-                        Links[idOfLink][6] = (int)1e5;
-                        Links[idOfLink][7] = (int)(1e9 * Links[idOfLink][6] / Links[idOfLink][9]);
-                        Links[idOfLink][8] = 1001;
-                        Links[idOfLink][12] = 0;
-                        Hosts[i][10] = -1;//xoa di thoi diem thuc thi su kien (C)
-                        Hosts[i][3] = -1;//xoa di goi tin o top cua outport
-                    }
+                    q[1]->id = curr / CYCLE_PACKET;//curr/CYCLE la id cua goi tin
+                    q[1]->hostID = hostID;
+                    q[1]->dstID = dst;
+                    q[1]->next = NULL;
                 }
-            }
-            //execute event B             
-            if(Hosts[i][11] == curr)
-            {
-                //Check the empty slot in outport
-                int slot = -1;
-                for(j = BUFFER_SIZE - 1; j >= 0; j--)
-                {
-                    if(Hosts[i][3 + j] == -1)
-                    {
-                        slot = j;
-                        break;
-                    }
+                else{
+                    Queue t = NULL;
+                    t = malloc(sizeof(Queue));
+                    t->id = curr / CYCLE_PACKET;//curr/CYCLE la id cua goi tin
+                    t->hostID = hostID;
+                    t->dstID = dst;
+                    t->next = NULL;
+                    q[1]->next = t;
+                    q[1] = t;
                 }
-
-                if(slot != -1)//if there is a empty slot
-                {
-                    Hosts[i][3 + slot] = Hosts[i][1];//push the packet to this slot
-                    if(Hosts[i][2] == -1)//if there is no remaining packet in source queue
-                       Hosts[i][1] = -1;//then the top of source queue is now empty
-                    else//otherwise
-                    {
-                        if(Hosts[i][2] == Hosts[i][1] + 1)//if only one remaining packet is in source queue
-                        {
-                            Hosts[i][2] = -1;//empty the source queue
-                        }
-                        Hosts[i][1]++;//push the following packet on the top
-                    }
-                    if(slot == 0)//neu toan bo outport cua Host trong
-                    {
-                        Hosts[i][10] = curr;//thiet lap cho viec gui goi tin
-                    }
-                }
-                else
-                {
-                    Hosts[i][11] == curr + RETRY_TIME;//Khong co outport nao trong, danh phai doi.
-                }
-            }
-            
-            
+            }   
         }
-        curr++;
+        //endof if(curr % CYCLE_PACKET == 0)
+
+        //execute event I
+        if(curr == timeOfI
+            )
+        {
+            if(sentID == receivedID && sentID != -1)
+            {
+                timeOfC = curr;//create event C
+                credit--;//thay doi gia tri credit
+            }
+            timeOfI = -1;
+        }
+
+        //execute event C
+        //NOTE: THIS CODE HAS A CLONE which begins at line 200
+        if( Links[idOfLink][2] != -1)//KHONG co goi tin tren duong truyen
+        {
+            //Link[index][0]: id cua nut nguon
+            //Link[index][1]: id cua nut dich
+            //Link[index][2]: id cua goi tin
+            //Link[index][3] = -1;/*id cua host nguon*/
+            //Link[index][4] = -1;/*id cua host dich*/
+            //Link[index][5] = 0;/*nix-vector*/
+            //Link[index][6] = 0;/*kich thuoc cua goi tin*/
+            //Link[index][7] = 0;/*thoi diem bit dau den*/
+            //Link[index][8] = 0;/*thoi diem bit cuoi den*/
+            //Link[index][9] = 0;/*bandwidth*/
+            //Link[index][10] = 0;/*bang 0 nghia la nut nguon ko phai la Host*/
+            //Link[index][11] = 0;/*bang 1 nghia la nut dich la Host*/
+            //Link[index][12] = 0;//Hop count
+
+            //int **outport; //an array with size = 2 x BUFFER_SIZE
+            //outport[0][i]: all ids of packets in outport
+            //outport[1][i]: all destinations of packets in outport
+
+            if(outport[0][0] != -1//Neu co goi tin o top cua outport
+                && timeOfC == curr //va goi tin o vi tri top nay co nhu cau duoc gui di
+                //&& Hosts[i][3] != -1
+                )
+            {
+                if(credit < BUFFER_SIZE)//neu inport cua switch tiep theo con cho trong
+                {
+                    //tao su kien tren link
+                    Links[idOfLink][2] = outport[0][0];//gan id cua goi tin vao Link
+                    Links[idOfLink][3] = hostID;//id cua host nguon
+                    Links[idOfLink][4] = outport[1][0];//id cua host dich
+                    Links[idOfLink][5] = path[1];//nix vector
+                    Links[idOfLink][6] = (int)1e5;
+                    Links[idOfLink][7] = (int)(1e9 * Links[idOfLink][6] / Links[idOfLink][9]);
+                    Links[idOfLink][8] = 1001;
+                    Links[idOfLink][12] = 0;
+                    timeOfC = -1;//xoa di thoi diem thuc thi su kien (C)
+                    credit ++;//thay doi gia tri credit
+                    for(j = 0; j < BUFFER_SIZE - 1; j++)
+                    {
+                        outport[0][j] = outport[0][j+1];
+                        outport[1][j] = outport[1][j+1];
+                    }
+                    outport[0][BUFFER_SIZE - 1] = -1;
+                    outport[1][BUFFER_SIZE - 1] = -1;
+                    timeOfB = curr;
+                }
+            }
+        }
+
+        //execute event B             
+        if(timeOfB == curr)
+        {
+            //Check the empty slot in outport
+            int slot = -1;
+            for(j = BUFFER_SIZE - 1; j >= 0; j--)
+            {
+                if(outport[0][j] == -1)
+                {
+                    slot = j;
+                    break;
+                }
+            }
+
+            if(slot != -1)//if there is a empty slot
+            {
+                outport[0][slot] = q[0]->id;//push the packet in the top source queue to this slot
+                outport[1][slot] = q[0]->dstID;
+                Queue next = q[0]->next;
+                if(next->id == q[1]->id)//if there is zero to one remaining packet in source queue
+                {    
+                    q[0] = q[0]->next;
+                    Queue t = NULL;
+                    t = malloc(sizeof(Queue));
+                    t->id = -1;
+                    t->dstID = -1;
+                    t->hostID = -1;
+                    t->next = 0;
+                    q[1] = t;
+                }
+                else//otherwise
+                {
+                    q[0] = q[0]->next;
+                }
+                if(slot == 0)//neu toan bo outport cua Host trong
+                {
+                    timeOfC = curr;
+                }
+            }
+            else
+            {
+                timeOfB = curr + RETRY_TIME;//Khong co outport nao trong, danh phai doi.
+            }
+        }
+
+
+        //execute event C
+        //NOTE: THE FOLLOWING CODE IS A CLONE OF lines between 103 and 151
+        if( Links[idOfLink][2] != -1)//KHONG co goi tin tren duong truyen
+        {
+            if(outport[0][0] != -1//Neu co goi tin o top cua outport
+                && timeOfC == curr //va goi tin o vi tri top nay co nhu cau duoc gui di
+                )
+            {
+                if(credit < BUFFER_SIZE)//neu inport cua switch tiep theo con cho trong
+                {
+                    //tao su kien tren link
+                    Links[idOfLink][2] = outport[0][0];//gan id cua goi tin vao Link
+                    Links[idOfLink][3] = hostID;//id cua host nguon
+                    Links[idOfLink][4] = outport[1][0];//id cua host dich
+                    Links[idOfLink][5] = path[1];//nix vector
+                    Links[idOfLink][6] = (int)1e5;
+                    Links[idOfLink][7] = (int)(1e9 * Links[idOfLink][6] / Links[idOfLink][9]);
+                    Links[idOfLink][8] = 1001;
+                    Links[idOfLink][12] = 0;
+                    timeOfC = -1;//xoa di thoi diem thuc thi su kien (C)
+                    credit ++;//thay doi gia tri credit
+                    for(j = 0; j < BUFFER_SIZE - 1; j++)
+                    {
+                        outport[0][j] = outport[0][j+1];
+                        outport[1][j] = outport[1][j+1];
+                    }
+                    outport[0][BUFFER_SIZE - 1] = -1;
+                    outport[1][BUFFER_SIZE - 1] = -1;
+                    timeOfB = curr;
+                }
+            }
+        }
+        
+        
     }
+    curr++;
 }
