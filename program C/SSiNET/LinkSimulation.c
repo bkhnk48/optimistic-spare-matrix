b53e2f160431f469bd0a5b6fb75b331f7b7f81ff
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-12-09 13:12:44 +0700

    new declaration for runLinks

--- program C/SSiNET/LinkSimulation.c
+++ program C/SSiNET/LinkSimulation.c
@@ -5,5 +5,16 @@
 
 void runLinks(Graph g, int *path, int curr)
 {
-
+    int i, j;
+    Host *Hosts = g->Hosts;
+    //int **Hosts;
+    int **Links = g->Links;
+    int *SwitchIndexes = g->SwitchIndexes;
+    int numOfLinks = g->numOfLinks;
+    int BUFFER_SIZE = g->BUFFER_SIZE;
+    int CYCLE_PACKET = g->CYCLE_PACKET;
+    int **MapFromNodesToPorts = g->MapFromNodesToPorts;
+    int minNextTime = INT_MAX;
+    int sizeOfShift = (8*sizeof(int)- 1);
+    
 }
