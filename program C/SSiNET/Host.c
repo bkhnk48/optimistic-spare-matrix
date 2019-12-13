77cdc58e0282d85c9299dd41f0c4840587bb406a
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-12-13 15:15:29 +0700

    get back to the name assignLinkID and remove redundant statement

--- program C/SSiNET/Host.c
+++ program C/SSiNET/Host.c
@@ -138,7 +138,7 @@ void assignHosts(Host *Hosts, int *IsHost, int numOfHosts, int BUFFER_SIZE)
     }
 }
 
-void assignLinkField(Host *Hosts, int **Links, int *IsHost, int numOfLinks, int Bandwidth)
+void assignLinkID(Host *Hosts, int **Links, int *IsHost, int numOfLinks)
 {
     int i, j; //int isHostAtDst = 0;
     int isHostAtSrc = 0, index = 0, count = 0; 
@@ -149,7 +149,7 @@ void assignLinkField(Host *Hosts, int **Links, int *IsHost, int numOfLinks, int 
         Links[i][2] = -1;//Khong co goi tin tren duong truyen
         //printf("\tidSrcNode = %d ", idSrcNode);
         isHostAtSrc = IsHost[idSrcNode];
-        Links[i][9] = Bandwidth;
+        //Links[i][9] = Bandwidth;
         Links[i][10] = IsHost[idSrcNode];
         Links[i][11] = IsHost[idDstNode];
         count += isHostAtSrc;
