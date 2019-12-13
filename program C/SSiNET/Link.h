1af71a24f5276a4e74d03bb7f8beb972513ea49c
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-12-13 15:16:44 +0700

    add new parameters to prototype of functions

--- program C/SSiNET/Link.h
+++ program C/SSiNET/Link.h
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "Link.c"
 
-void addLink(int **Link, int **adjacentMatrix, int width, int numOfLinks);
+void addLink(int **Link, int **adjacentMatrix, int width, int numOfLinks, int serialTime, int propagationTime);
 
 void showLink(int **Links, int count);
 
-void assignLink(int **Link, int k);
+void assignLink(int **Links, int k, int serialTime, int propagationTime);
 
 void showLinksInGraph(Graph graph);
 
