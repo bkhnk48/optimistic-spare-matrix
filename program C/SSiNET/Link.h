268675cc5bd8ecad148e352aff4795e0e889482a
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2019-12-13 17:37:46 +0700

    add new prototype of function

--- program C/SSiNET/Link.h
+++ program C/SSiNET/Link.h
@@ -7,5 +7,9 @@ void showLink(int **Links, int count);
 
 void assignLink(int **Links, int k, int serialTime, int propagationTime);
 
+void assignPortInfo(int **MapFromNodesToPorts, int **Links, 
+                            int *SwitchIndexes, 
+                            int numOfLinks, int numOfPorts);
+
 void showLinksInGraph(Graph graph);
 
