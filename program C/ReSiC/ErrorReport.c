6c27a433baacf230ba7927c3653ede38dcee6f38
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-03-29 12:15:27 +0700

    file for error notification

--- program C/ReSiC/ErrorReport.c
+++ program C/ReSiC/ErrorReport.c
@@ -3,11 +3,17 @@
 void nullPointerException(int line, char *fileName, const char *funcName);
 void outOfBoundException(int line, char *fileName, const char *funcName);
 
+/*
+    This method prints error message about null pointer exception
+*/
 void nullPointerException(int line, char *fileName, const char *funcName)
 {
     printf("\n NULL Pointer at here: %d in file: %s in function: %s", line, fileName, funcName);
 }
 
+/*
+    This method prints error message about out of boundary exception
+*/
 void outOfBoundException(int line, char *fileName, const char *funcName)
 {
     printf("\n OUT of Boundary Exception at here: %d in file: %s in function: %s", line, fileName, funcName);
