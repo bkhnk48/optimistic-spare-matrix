96794b00baae2b12e7317c09ffa9e6e06ad9503a
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-05-05 18:27:51 +0700

    add new function

--- program C/ReSiC/ShiftBit.c
+++ program C/ReSiC/ShiftBit.c
@@ -14,3 +14,23 @@ int checkEqual(int a, int b)
     y = 1 + y;
     return y;
 }
+
+//Ham nay dua tren mot gia thuyet rang:
+// kich thuoc cua buffer_size la gioi han
+int checkUpdateEXBHost(int topID, int bottomID, int BUFFER_SIZE)
+{
+    int isEmptyEXB = -(topID >> 31);//chi nhan gia tri 0 hoac 1.
+    
+    int indexOfUpdate = (bottomID - topID + 1);
+    
+    int isFullEXB = indexOfUpdate - BUFFER_SIZE;
+    isFullEXB = 1 + (isFullEXB >> 31); //0 nghia la EXB chua full, 1 nghia la EXB da full.
+    
+    int allowUpdate = 1 - isFullEXB; //0 nghia la khong cho update, 1 nghia la cho update
+    
+    indexOfUpdate = (1 - isEmptyEXB)*indexOfUpdate*allowUpdate;
+
+    indexOfUpdate = indexOfUpdate << 1;
+    int result = indexOfUpdate & isEmptyEXB;
+    return result;
+}
