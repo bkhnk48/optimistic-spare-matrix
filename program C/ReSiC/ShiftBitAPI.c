80bde894a15be326926e400da2c2c82d42969620
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-05-28 11:08:38 +0700

    new implementation of checkUpdateEXBHost

--- program C/ReSiC/ShiftBitAPI.c
+++ program C/ReSiC/ShiftBitAPI.c
@@ -17,15 +17,21 @@ int checkEqual(int a, int b)
 
 //Ham nay dua tren mot gia thuyet rang:
 // kich thuoc cua buffer_size la gioi han
-int checkUpdateEXBHost(int topID, int bottomID, int BUFFER_SIZE)
+int checkUpdateEXBHost(int topEXB, int topSQ, int BUFFER_SIZE)
 {
-    int isEmptyEXB = -(topID >> 31);//chi nhan gia tri 0 hoac 1.
+    //Neu topEXB co id = 1 va topSQ co id = 3
+    //Vay thi trong EXB da co 2 pkt voi id = 1 va 2.
+    //do EXBHost chua id cua goi tin (tai cac phan tu co chi so chan)
+    //con EXBHost chua ca dst cua goi tin (tai cac phan tu co chi so le)
+    //nen chi so cua EXBHost can cap nhat ID goi tin se la 2*(3 - 1) = 4;
+    int isEmptyEXB = -(topEXB >> 31);//chi nhan gia tri 0 hoac 1.
 
-    int isAlreadyOne = -(topID*bottomID >> 31); //0: khong phai chi chua 1 phan tu. 1: chi chua 1 phan tu
+    //int isAlreadyOne = -(topID*bottomID >> 31); //0: khong phai chi chua 1 phan tu. 1: chi chua 1 phan tu
     
-    int indexOfUpdate = (bottomID - topID + 1)*(1 - isAlreadyOne) + isAlreadyOne;
+    //int indexOfUpdate = (bottomID - topID + 1)*(1 - isAlreadyOne) + isAlreadyOne;
+    int indexOfUpdate = 2*(topSQ - topEXB);
     
-    int isFullEXB = indexOfUpdate - BUFFER_SIZE;
+    int isFullEXB = indexOfUpdate - 2*(BUFFER_SIZE - 1);
     isFullEXB = 1 + (isFullEXB >> 31); //0 nghia la EXB chua full, 1 nghia la EXB da full.
     
     int allowUpdate = 1 - isFullEXB; //0 nghia la khong cho update, 1 nghia la cho update
