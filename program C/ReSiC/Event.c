24fc74e1f2e72b1fc2ae0ce0ca870b7c954d8230
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-09-19 17:00:45 +0700

    add Event.c for types of events

---
+++ program C/ReSiC/Event.c
@@ -0,0 +1,11 @@
+enum TypesOfEvent
+{
+    A, //GenerationEvent
+    B, //LeavingSourceQueueEvent
+    C, //LeavingEXBEvent
+    D, //ReachingENBEvent
+    E, //MovingInSwitchEvent
+    F, //LeavingSwitchEvent
+    G, //ReachingDestinationEvent
+    H  //NotificationEvent
+};
