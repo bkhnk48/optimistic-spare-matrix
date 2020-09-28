6699e7c35c5057c2b6f56cc8ccb70ee334676cbf
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-09-28 23:55:59 +0700

    check performance as passing list of int and struct

--- program C/ReSiC/TestPerformance.c
+++ program C/ReSiC/TestPerformance.c
@@ -6,6 +6,9 @@ typedef struct NODE{
     int * right;
     int * father;
 } Node;
+typedef struct VALUE{
+    int a, b, c, d, e, f, g;
+} Value;
 
 void initArray(int k)
 {
@@ -81,6 +84,39 @@ void mallocOneNode(int *i, Node * node)
     node->right = NULL;
     node->father = NULL;
 }
+
+void passIntegers(int x, int y, int z, 
+                    int u, int v, int r, int s, 
+                    Node *t)
+{
+    if(t == NULL)
+    {
+        t = (Node *)malloc(sizeof(Node));
+    }
+    t->a = x;
+    t->b = y;
+    t->c = z;
+    t->d = u;
+    t->left = NULL;
+    t->right = NULL;
+    t->father = NULL;
+}
+
+void passStruct(Value v, Node *t)
+{
+    if(t == NULL)
+    {
+        t = (Node *)malloc(sizeof(Node));
+    }
+    t->a = v.a + v.e;
+    t->b = v.b + v.f;
+    t->c = v.c + v.g;
+    t->d = v.d;
+    t->left = NULL;
+    t->right = NULL;
+    t->father = NULL;
+}
+
 void main(int argc, char** argv) {
     int att = atoi(argv[1]);
 
@@ -120,6 +156,52 @@ void main(int argc, char** argv) {
         printf("Time: %f ms\n", (wc2 - wc1)*1000);
         printf("================================\n");
         double loadedBytes = (double)7*4*numOfInitialization/(1000*1000);
+        printf("Performance %lf MB/s\n", loadedBytes/(wc2 - wc1));
+        return;
+    }
+
+    if(att == 4)
+    {
+        int numOfInitialization = 320;
+        int i = 0;
+        double wc1 = 0, wc2 = 0, cpuT = 0;
+
+
+        timing(&wc1, &cpuT);
+        Node *t = NULL;
+        t = (Node *)malloc(sizeof(Node));
+        Value v;
+        v.a = 5; v.b = 6; v.c = 7; v.d = 8;
+        v.e = 1; v.f = 5; v.g = 2;
+        while (i < numOfInitialization)
+        {
+            passStruct(v, t);
+            i++;
+            if(i < 0) { v.a ++;}
+        }
+
+        timing(&wc2, &cpuT);
+        printf("Time passStruct: %f ms\n", (wc2 - wc1)*1000);
+        printf("================================\n");
+        double loadedBytes = (double)7*4*numOfInitialization/(1000*1000);
+        printf("Performance %lf MB/s\n", loadedBytes/(wc2 - wc1));
+
+        wc1 = 0; wc2 = 0; cpuT = 0;
+        timing(&wc1, &cpuT);
+        t = (Node *)malloc(sizeof(Node));
+        i = 0;
+        int x = 8, y = 9, z = 10, u = 8;
+        while (i < numOfInitialization)
+        {
+            passIntegers(x, y, z, u, x + 2, y + 2, z + 4, t);
+            i++;
+            if(i < 0) { x ++;}
+        }
+
+        timing(&wc2, &cpuT);
+        printf("Time passIntegers: %f ms\n", (wc2 - wc1)*1000);
+        printf("================================\n");
+        
         printf("Performance %lf MB/s\n", loadedBytes/(wc2 - wc1));
         return;
     }
