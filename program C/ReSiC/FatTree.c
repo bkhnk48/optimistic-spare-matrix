bd724c7783d52a260ae5e43d7ac1ae08f57e4352
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-09-29 23:00:33 +0700

    replace if by switch

--- program C/ReSiC/FatTree.c
+++ program C/ReSiC/FatTree.c
@@ -713,12 +713,13 @@ int main(int argc, char** argv)
     {
         if(ev->endTime == currentTime)
         {
+            i = ev->idElementInGroup;//Lay id cua host trong danh sach cac hosts
+            j = ev->idNode;//lay id cua node trong danh sach cac nodes
+                        
             switch(ev->type)
             {
                 case A:
                     {
-                        i = ev->idElementInGroup;//Lay id cua host trong danh sach cac hosts
-                        j = ev->idNode;
                         int isEmptySQ = -(PacketInSQ[i][0] >> 31);//kiem tra xem SQ co empty ko?
                         //int indexOfUpdateSQ = 1 + (-PacketInSQ[i][0] + PacketInSQ[i][1] + 1);//cap nhat phan tu nao cua PacketInSQ?
                         //indexOfUpdateSQ *= isEmptySQ;//cap nhat phan tu nao cua PacketInSQ?
@@ -733,72 +734,64 @@ int main(int argc, char** argv)
                         //check if the EXB has no empty slot:
                         int isNotFullEXB = PacketInEXBHost[i][1] - PacketInEXBHost[i][0] + 1 - BUFFER_SIZE;
                         isNotFullEXB = -(isNotFullEXB >> 31); //1 nghia la EXB chua full, 0 nghia la EXB da full.
-                        switch(isNotFullEXB)
+                        if(isNotFullEXB == 1)//event B co the duoc tao ra
                         {
-                            case 1://event B co the duoc tao ra
-                                rootHosts = add(B, //type B
-                                        idOfNewPkt, //packetID 
-                                        j,
-                                        i, //location at this host
-                                        currentTime, //startTime = currentTime 
-                                        currentTime, //endTime = currentTime (right now)
-                                        rootHosts);
-                                show(rootHosts);
-                                break;
+                            rootHosts = add(B, //type B
+                                    idOfNewPkt, //packetID 
+                                    j,
+                                    i, //location at this host
+                                    currentTime, //startTime = currentTime 
+                                    currentTime, //endTime = currentTime (right now)
+                                    rootHosts);
+                            show(rootHosts);
                         }
                     }
                     break;
                 case B:
                     {
-                        i = ev->idElementInGroup;//Lay id cua host trong danh sach cac hosts
-                        j = ev->idNode;//lay id cua node trong danh sach cac nodes
                         int isEmptySQ = -(PacketInSQ[i][0] >> 31);//kiem tra xem SQ co empty ko?
                         
                         int isNotFullEXB = PacketInEXBHost[i][1] - PacketInEXBHost[i][0] + 1 - BUFFER_SIZE;
                         isNotFullEXB = -(isNotFullEXB >> 31); //1 nghia la EXB chua full, 0 nghia la EXB da full.
-                        switch((1-isEmptySQ)*isNotFullEXB)
+                        if((1-isEmptySQ)*isNotFullEXB == 1)
                         {
-                            case 1: 
-                                {
-                                    int isEmptyEXB = -(PacketInEXBHost[i][0] >> 31);//chi nhan gia tri 0 hoac 1.
-                                    int indexOfUpdate = (1 - isEmptyEXB);
-                                    PacketInEXBHost[i][indexOfUpdate] = PacketInSQ[i][0];
-                                    ///III. Remove the first packet of Source queue
-                                    //After insert the packet into the empty slot of EXB (in host)
-                                    //all remaining packets in source queue need to be moved forward
-                                    ///If there was only one packet in SQ
-                                    int isOnePkt = -(PacketInSQ[i][1] >> 31); 
-                                    //0 nghia la co hon 1 goi tin trong SQ. 1 nghia la chi co 1 goi tin trong SQ
-                                    //2 cau lenh duoi day cap nhat lai id cua cac goi tin trong SQ
-                                    PacketInSQ[i][0] = -isOnePkt + (1-isOnePkt)*(PacketInSQ[i][0] + 1);
-                                    PacketInSQ[i][1] = -isOnePkt + (1-isOnePkt)*(PacketInSQ[i][1]);
+                                
+                            int isEmptyEXB = -(PacketInEXBHost[i][0] >> 31);//chi nhan gia tri 0 hoac 1.
+                            int indexOfUpdate = (1 - isEmptyEXB);
+                            PacketInEXBHost[i][indexOfUpdate] = PacketInSQ[i][0];
+                            ///III. Remove the first packet of Source queue
+                            //After insert the packet into the empty slot of EXB (in host)
+                            //all remaining packets in source queue need to be moved forward
+                            ///If there was only one packet in SQ
+                            int isOnePkt = -(PacketInSQ[i][1] >> 31); 
+                            //0 nghia la co hon 1 goi tin trong SQ. 1 nghia la chi co 1 goi tin trong SQ
+                            //2 cau lenh duoi day cap nhat lai id cua cac goi tin trong SQ
+                            PacketInSQ[i][0] = -isOnePkt + (1-isOnePkt)*(PacketInSQ[i][0] + 1);
+                            PacketInSQ[i][1] = -isOnePkt + (1-isOnePkt)*(PacketInSQ[i][1]);
 
-                                    ///Kiem tra xem co tao event C duoc hay khong?
-                                    //Cach kiem tra: CounterH > 0 va WayHE[i][2] != 1.
-                                    int checkWayHE = (WayHE[i][2] - 1);
-                                    checkWayHE *= checkWayHE;
-                                    int leaveEXB = -((-CounterH[i]) >> 31)*checkWayHE;
-                                    switch (leaveEXB)
-                                    {
-                                        case 1:
-                                            rootHosts = add(C, //type B
-                                                        PacketInEXBHost[i][0], //packetID 
-                                                        j,
-                                                        i, //location of this host
-                                                        currentTime, //startTime = currentTime 
-                                                        currentTime, //endTime = currentTime (right now)
-                                                        rootHosts);
-                                            break;
-                                    }
-                                }
-                                break;
+                            ///Kiem tra xem co tao event C duoc hay khong?
+                            //Cach kiem tra: CounterH > 0 va WayHE[i][2] != 1.
+                            int checkWayHE = (WayHE[i][2] - 1);
+                            checkWayHE *= checkWayHE;
+                            int leaveEXB = -((-CounterH[i]) >> 31)*checkWayHE;
+                            switch (leaveEXB)
+                            {
+                                case 1:
+                                    rootHosts = add(C, //type B
+                                                PacketInEXBHost[i][0], //packetID 
+                                                j,
+                                                i, //location of this host
+                                                currentTime, //startTime = currentTime 
+                                                currentTime, //endTime = currentTime (right now)
+                                                rootHosts);
+                                    break;
+                            }
                         }
+                        
                     }
                     break;
                 case C:
                     {
-                        i = ev->idElementInGroup;//Lay id cua host trong danh sach cac hosts
-                        j = ev->idNode;//lay id cua node trong danh sach cac nodes
                         
                     }
                     break;
