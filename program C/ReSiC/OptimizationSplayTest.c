bad18d470fede3595b5164a3dbb5e7f4803f59fe
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-10-07 15:27:18 +0700

    prepare for new unit test

--- program C/ReSiC/OptimizationSplayTest.c
+++ program C/ReSiC/OptimizationSplayTest.c
@@ -20,6 +20,42 @@ int main(int argc, char** argv)
 
    timing(&wc1, &cpuT);
 
+   root = -1;
+
+   for(i = 0; i < 16; i++)
+   {
+      add(0, i, 0, 0, &root, arr);
+   }
+   
+   /*if(i < 0){
+      arr[21][7] += i + N;
+   }*/
+   
+
+   timing(&wc2, &cpuT);
+   printf("Time: %f ms\n", (wc2 - wc1)*1000);
+   printf("================================\n");
+   //show(arr, root); 
+   return 0;
+}
+
+void testGeneratingEventA()
+{
+   double wc1 = 0, wc2 = 0, cpuT = 0;
+
+
+   int arr[384][7];
+   int i, j, N, root = -1;
+   for(i = 0; i < 384; i++)
+   {
+      for(j = 0; j < 7; j++)
+      {
+         arr[i][j] = -1;
+      }
+   }
+
+   timing(&wc1, &cpuT);
+
    for(N = 0; N < 1000; N++)
    {
       
@@ -28,7 +64,7 @@ int main(int argc, char** argv)
       for(i = 0; i < 16; i++)
       {
          add(0, i, 0, ((i + N) % 2), &root, arr);
-         //add(0, 1, 0, 0, &root, arr);
+         
       }
       if(i < 0){
          arr[21][7] += i + N;
@@ -38,6 +74,4 @@ int main(int argc, char** argv)
    timing(&wc2, &cpuT);
    printf("Time: %f ms\n", (wc2 - wc1)*1000);
    printf("================================\n");
-   //show(arr, root); 
-   return 0;
 }
