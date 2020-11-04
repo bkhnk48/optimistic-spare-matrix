d6660b81531707e37f74628a5d2b235e2993a75b
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-11-05 06:51:09 +0700

    update new implementation

--- program C/calendarqueue/Node.c
+++ program C/calendarqueue/Node.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
@@ -27,34 +26,4 @@ node* new_node(int type, int idElementInGroup,
   n->next = NULL;
 
   return n;
-=======
-#include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
-#include "Event.c"
-
-typedef struct node {
-    enum TypesOfEvent type; //type of event
-    
-    int idElementInGroup;//id of element in group of hosts or switches
-    int portID;
-    int endTime;
-    struct node * next;
-} node;
-
-node* new_node(int type, int idElementInGroup,
-                int portID, 
-                int endTime) {
-  node *n = (node *)malloc(sizeof(node));
-  if (!n) {
-    return NULL;
-  }
-  n->type = type;
-  n->idElementInGroup = idElementInGroup;
-  n->portID = portID;
-  n->endTime = endTime;
-  n->next = NULL;
-
-  return n;
->>>>>>> 9cf267c1f2f5a4717098c8415b480f5a54ebd8fa
 }
