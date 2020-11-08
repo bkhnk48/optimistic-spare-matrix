3173bb2b191e86b592d08b3b413a5099bcce4854
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-11-08 11:53:34 +0700

    replace negative numbers to -1

--- program C/experiment/randomFile.c
+++ program C/experiment/randomFile.c
@@ -6,16 +6,14 @@ int my_random(int minN, int maxN){
     return minN + rand() % (maxN + 1 - minN);
 }
 
-
-
 int main(){
     srand((int)time(0));
     FILE *fptr;
     fptr = fopen("random1000.txt","w");
     int r;
     for(int i = 0; i < 1000; ++i){
-        r = my_random(1,100);
-        if(r <= 21) r = 0;
+        r = my_random(-10,100);
+        if(r < 0) r = -1;
         fprintf(fptr,"%d\n", r);
 
     }
