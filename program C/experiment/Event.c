14bb8641a775411d93d0739beb2d48936a6d4d7e
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-11-08 09:54:45 +0700

    add loadArray function

--- program C/experiment/Event.c
+++ program C/experiment/Event.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #ifndef _TYPES_OF_EVENT_
 #define _TYPES_OF_EVENT_
 enum TypesOfEvent
@@ -15,4 +17,22 @@ enum TypesOfEvent
 };
 
 enum Side{LEFT, RIGHT};
+
+void loadArray(int a[1000]){
+   int num;
+   FILE *fptr;
+
+   if ((fptr = fopen("random1000.txt","r")) == NULL){
+       printf("Error! opening file");
+       return;
+   }
+   int i = 0;
+   while(!feof(fptr)){
+    fscanf(fptr,"%d", &num);
+    a[i] = num;
+    i++;
+   }
+
+   fclose(fptr);
+}
 #endif
