21a737f534cd1c7a343eafe4ac0fb1be4e8bbbe5
Tien Thanh <tienthanh@ubuntu.ubuntu-domain>
2020-11-08 09:16:44 +0700

    use enqueue and dequeue instead of insert, remove

--- program C/experiment/CalendarQueueTest.c
+++ program C/experiment/CalendarQueueTest.c
@@ -1,48 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "CalendarQueue.c"
 #include "timing.c"
 
 int main()
 {
-    List POOL[QSPACE];
-    CalendarQueue *q = initqueue(POOL);
+    initqueue();
     double wc1 = 0, wc2 = 0, cpuT = 0;
     int i = 0;
     long count = 0;
     timing(&wc1, &cpuT);
-    unsigned long currentTime = 0;
-    unsigned long endTime = 10*((unsigned long)(1000*1000));
+    double currentTime = 0;
+    double endTime = 1000*1000;
 
     for(i = 0; i < 6750; i++)
     {
-        insert(q, new_node(A, i, 0, 0), POOL);
+        enqueue(new_node(A, i, 0, 0));
     }
-    
-    Node* ev = dequeue(q, POOL);
+
+
+    Node * ev = dequeue();
     while(currentTime <= endTime && ev->endTime != -1)
-    {
+    {   //printf("hello world");
         if(ev->endTime == currentTime)
         {
             count++;
-            //printf("%d)Event type = %d at %d with endTime = %d\n", count, ev->type, ev->idElementInGroup, ev->endTime);
+            /*printf("%d)Event type = %d at %d with endTime = %d\n"
+                , count, ev->type, ev->idElementInGroup, ev->endTime
+                    );*/
             i = ev->idElementInGroup;//Lay id cua host trong danh sach cac hosts
             if(ev->type == A)
             {
-                insert(q, new_node(A, i, 0, currentTime + 10000), POOL);
-                insert(q, new_node(B, i, 0, currentTime), POOL);
+                enqueue(new_node(A, i, 0, currentTime + 100));
+                enqueue(new_node(B, i, 0, currentTime));
             }
             else if(ev->type == B)
             {
-                insert(q, new_node(C, i, 0, currentTime), POOL);
+                enqueue(new_node(C, i, 0, currentTime));
             }
             ev->endTime = -1;
-            ev = dequeue(q, POOL);
+            ev = dequeue();
 
             currentTime = ev->endTime;
         }
     }
-    
+
     timing(&wc2, &cpuT);
-	
     printf("Time: %f ms with count = %ld\n", (wc2 - wc1)*1000, count);
     printf("================================\n");
 
