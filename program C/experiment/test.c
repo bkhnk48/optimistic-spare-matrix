80c321a8feeea36ea46bf3bd064c2e7205a9e106
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2020-11-15 03:12:57 -0500

    check page fault

--- program C/experiment/test.c
+++ program C/experiment/test.c
@@ -11,6 +11,7 @@ void dummy(int * x, int * max, int * min)
 int main(int argc, char** argv)
 {
     double wc1 = 0, wc2 = 0, cpuT = 0;
+    long pageFault1, pageFault2;
     double value1, value2;
     int i =0;
     char *input1 = argv[1];
@@ -20,7 +21,7 @@ int main(int argc, char** argv)
     int max = atoi(input2);
     int min = atoi(input3);
     int sum = 1;
-    timing(&wc1, &cpuT);
+    timing(&wc1, &cpuT, &pageFault1);
     for(i = 0; i < 1000000; i++)
     {
         if(x >= min && x < max)
@@ -31,14 +32,14 @@ int main(int argc, char** argv)
                 for Scientist and Engineers", page 5
         */
     }
-    timing(&wc2, &cpuT);
+    timing(&wc2, &cpuT, &pageFault2);
     value1 = (wc2 - wc1)*1000;
     printf("Time of if(x >= min && x < max): %f ms\n", value1);
     printf("================================\n");
     sum = 1;
     i = 0;
     x++;
-    timing(&wc1, &cpuT);
+    timing(&wc1, &cpuT, &pageFault1);
     for(i = 0; i < 1000000; i++)
     {
         if((unsigned)(x - min) < (max - min))
@@ -49,7 +50,7 @@ int main(int argc, char** argv)
                 for Scientist and Engineers", page 5
         */
     }
-    timing(&wc2, &cpuT);
+    timing(&wc2, &cpuT, &pageFault2);
     value2 = (wc2 - wc1)*1000;
     printf("Time of if((unsigned)(x - min) < (max - min)): %f ms\n", value2);
     printf("================================\n");
