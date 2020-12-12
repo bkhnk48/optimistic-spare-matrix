c873b9ffbf5e2ddba4f1775e4e97e04f0f71e701
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2020-12-12 09:15:06 +0700

    remove some redundant comments and call badness function

--- program C/experiment/CalendarQueueTest.c
+++ program C/experiment/CalendarQueueTest.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "CalendarQueue.c"
 #include "timing.c"
+#include <math.h>
 
 
 int main(int argc, char** argv) 
@@ -16,10 +17,15 @@ int main(int argc, char** argv)
     unsigned long index = 0;
     
     int defaultSec = 70;
+    int defaultBias = 1;
+    ProcStatm proc_statm;
+    long page_size = sysconf(_SC_PAGESIZE);
 
     if(argc >= 2)
     {
       defaultSec = atoi(argv[1]);
+      if(argc >= 3)
+        defaultBias = atoi(argv[2]);
     }
     unsigned long currentTime = 0;
     unsigned long endTime = defaultSec*((unsigned long)(1000*1000));
@@ -41,35 +47,23 @@ int main(int argc, char** argv)
     
     Node * ev = dequeue();
     while(currentTime <= endTime && ev->endTime != -1)
-    {   //printf("hello world");
-        //for (int i = 0; i < 1024; ++i) {
-        //   p[i * 1024 * 1024] = currentTime;    
-        //}
+    {   
         if(ev->endTime == currentTime)
         {
             count++;
-            /*printf("%d)Event type = %d at %d with endTime = %d\n"
-                , count, ev->type, ev->idElementInGroup, ev->endTime
-                    );*/
+            
             i = ev->idElementInGroup;//Lay id cua host trong danh sach cac hosts
             if(ev->type == A)
             {
                 enqueue(new_node(A, i, 0L, currentTime + 10000));
-                enqueue(new_node(B, i, 0L, currentTime +  13
+                enqueue(new_node(B, i, 0L, currentTime + defaultBias*13
                                         ));
             }
             else if(ev->type == B)
             {
-                enqueue(new_node(C, i, 0L, currentTime + 33
+                enqueue(new_node(C, i, 0L, currentTime + defaultBias*33
                                         ));
             }
-            //int temp = arbitrary[index % 1000];
-            //if(temp != -1)
-            {
-                //enqueue(new_node(C, i, 0, currentTime + temp));
-                //printf("%ld ", currentTime + temp);
-            }
-            //index++;
             ev->endTime = -1;
             ev = dequeue();
 
@@ -81,6 +75,7 @@ int main(int argc, char** argv)
     timing(&wc2, &cpuT);
     printf("Time: %'f ms with count = %'ld\n", (wc2 - wc1)*1000, count);
     printf("================================\n");
+    badness(wc2 - wc1, page_size, proc_statm);
 
     return 0;
 }
