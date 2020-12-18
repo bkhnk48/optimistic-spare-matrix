f9049c28102eb2a8416fd0d3e0bb831b6ca38b24
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2020-12-18 11:48:02 +0700

    idenfify address of core neighbor

--- program C/experiment/FatTreeTables.c
+++ program C/experiment/FatTreeTables.c
@@ -78,7 +78,10 @@ int getNeighborIP(int currentIP, enum TypesOfNode typeOfNode, int port, int k){
       return neighborIP;
     }
     else{//neighbor is core switch
-
+      int indexOfAggInPod = _switch % (k/2);
+      int core = indexOfAggInPod*(k/2) + port + (k*k*k/4);
+      neighborIP = getIPv4OfSwitch(core, k);
+      return neighborIP;
     }  
   }
   #pragma endregion
