2cd5596ce83dcc1806ccb47c6ede5a686d4a5926
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2020-12-26 11:22:14 +0700

    build tables for edge and agg switches

--- program C/experiment/FatTreeTables.c
+++ program C/experiment/FatTreeTables.c
@@ -16,47 +16,6 @@ typedef struct _tables{
   RoutingTable *tables;
 }Tables;
 
-void buildTables(Tables *tablesOfSwitches, int k){
-  //In Fat-tree, build total k*k*5/4 routing tables
-  tablesOfSwitches->tables = (RoutingTable *)malloc((k*k*5/4)*sizeof(RoutingTable));
-  int i, j ;
-  int numOfSwitches = k*k*5/4;
-  #pragma region initialization
-  for(i = 0; i < numOfSwitches; i++){
-    if(i < k*k){
-      if((i % k) >= 0 && (i %k) <= (k/2 - 1)){
-        tablesOfSwitches->tables[i].type = EDGE_SWITCH;
-        tablesOfSwitches->tables[i].prefixTable = NULL;
-        tablesOfSwitches->tables[i].subPrefix = 0;
-        int ipv4 = getIPv4OfSwitch(i, k);
-        tablesOfSwitches->tables[i].suffixTable = 
-              (int *)malloc(k*sizeof(int));
-        for(j = 0; j < k; j++){
-          tablesOfSwitches->tables[i].suffixTable[j]
-            = getNeighborIP(ipv4, EDGE_SWITCH, j, k);
-        }
-        
-      }
-      else{
-        tablesOfSwitches->tables[i].type = AGG_SWITCH;
-        tablesOfSwitches->tables[i].subPrefix = 0;
-        int ipv4 = getIPv4OfSwitch(i, k);
-        tablesOfSwitches->tables[i].suffixTable = 
-              (int *)malloc(k*sizeof(int));
-        for(j = 0; j < k; j++){
-          tablesOfSwitches->tables[i].suffixTable[j]
-            = getNeighborIP(ipv4, AGG_SWITCH, j, k);
-        }
-      }
-    }
-    else{
-      tablesOfSwitches->tables[i].type = CORE_SWITCH;
-      tablesOfSwitches->tables[i].suffixTable = NULL;
-    }
-  }
-  #pragma endregion
-}
-
 int getNeighborIP(int currentIP, enum TypesOfNode typeOfNode,
                       int port, int k){
   int neighborIP;
@@ -104,5 +63,58 @@ int getNeighborIP(int currentIP, enum TypesOfNode typeOfNode,
   #pragma endregion
   return neighborIP;
 }
+
+
+void buildTables(Tables *tablesOfSwitches, int k){
+  //In Fat-tree, build total k*k*5/4 routing tables
+  tablesOfSwitches->tables = (RoutingTable *)malloc((k*k*5/4)*sizeof(RoutingTable));
+  int i, j ;
+  int numOfSwitches = k*k*5/4;
+  #pragma region initialization
+  for(i = 0; i < numOfSwitches; i++){
+    if(i < k*k){
+      #pragma region Build tables for edge switches
+      if((i % k) >= 0 && (i %k) <= (k/2 - 1)){
+        tablesOfSwitches->tables[i].type = EDGE_SWITCH;
+        tablesOfSwitches->tables[i].prefixTable = NULL;
+        tablesOfSwitches->tables[i].subPrefix = 0;
+        int ipv4 = getIPv4OfSwitch(i, k);
+        tablesOfSwitches->tables[i].suffixTable = 
+              (int *)malloc((k/2)*sizeof(int));
+        for(j = 0; j < k/2; j++){
+          tablesOfSwitches->tables[i].suffixTable[j]
+            = getNeighborIP(ipv4, EDGE_SWITCH, j, k);//AGG SWITCH
+        }
+      }
+      #pragma endregion
+      else{
+      #pragma region Build tables for agg switch
+        tablesOfSwitches->tables[i].type = AGG_SWITCH;
+        int ipv4 = getIPv4OfSwitch(i, k);
+        tablesOfSwitches->tables[i].suffixTable = 
+              (int *)malloc((k/2)*sizeof(int));
+        for(j = 0; j < k/2; j++){
+          tablesOfSwitches->tables[i].suffixTable[j]
+            = getNeighborIP(ipv4, AGG_SWITCH, j, k);//CORE SWITCH
+        }
+        int p = i/k;
+        tablesOfSwitches->tables[i].subPrefix = (10 << 16) | (p << 8);
+        tablesOfSwitches->tables[i].prefixTable = 
+              (int *)malloc((k/2)*sizeof(int));
+        for(j = 0; j < k/2; j++){
+          tablesOfSwitches->tables[i].prefixTable[j]
+            = getNeighborIP(ipv4, AGG_SWITCH, j, k);//EDGE SWITCH
+        }
+      #pragma endregion  
+      }
+    }
+    else{
+      tablesOfSwitches->tables[i].type = CORE_SWITCH;
+      tablesOfSwitches->tables[i].suffixTable = NULL;
+    }
+  }
+  #pragma endregion
+}
+
 #endif
 
