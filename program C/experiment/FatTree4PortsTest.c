d3dbcd762e2408e4dd30df0e6765aa3111d23753
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-01-01 01:03:58 +0700

    initialize variables

--- program C/experiment/FatTree4PortsTest.c
+++ program C/experiment/FatTree4PortsTest.c
@@ -28,4 +28,29 @@ int main(int argc, char** argv) {
     unsigned long currentTime = 0;
     unsigned long endTime = defaultSec*((unsigned long)(1000*1000));
 
+    unsigned long arr[384][7];//384 = 6*(k*k*k) as k = 4
+    unsigned long i;
+    int j, N, root = -1;
+    for(i = 0; i < 384; i++)
+    {
+      for(j = 0; j < 7; j++)
+      {
+         arr[i][j] = -1L;
+      }
+    }
+    setlocale(LC_NUMERIC, "");
+   
+    printf("Simulation time is %ld (s)\n", endTime / (1000*1000));
+    unsigned long mem = mem_avail();
+    printf("Free memory Available = %'ld\n", mem / (1024*1024));
+
+    printf("Start Simulating ......\n");
+    unsigned long count = 0;
+    //char* p = malloc(1 * 1024 * 1024 * 1024);
+    timing(&wc1, &cpuT);
+
+    root = -1;
+
+    return 0;
+
 }
