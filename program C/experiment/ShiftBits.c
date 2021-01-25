ae4bc22e1580941e991543f6d8fc0e258b0800d1
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-01-26 06:28:57 +0700

    add three new functions

--- program C/experiment/ShiftBits.c
+++ program C/experiment/ShiftBits.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
+#ifndef _LIB_OF_SHIFT_BITS_
+#define _LIB_OF_SHIFT_BITS_
+
+int getCount(unsigned long id){
+    return (id & USHRT_MAX);
+}
+
+int getMin(unsigned long id){
+    return ((id >> 16) & USHRT_MAX);
+}
+
+int getMax(unsigned long id){
+    return ((id >> 32) & USHRT_MAX);
+}
+#endif
