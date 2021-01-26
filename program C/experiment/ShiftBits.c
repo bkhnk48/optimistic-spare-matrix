e1c6879f8302cdee49a18d0d7070df66385d312d
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-01-26 09:51:11 +0700

    implement the func increase

--- program C/experiment/ShiftBits.c
+++ program C/experiment/ShiftBits.c
@@ -24,6 +24,27 @@ int increase(unsigned long id, int portENB){
         id |= (portENB << 16);
         return id;
     }
+    else{
+        int count = getCount(id);
+        count++;
+        int min = getMin(id);
+        int max = getMax(id);
+        if(max == 0)
+            max = min;
+        if(max < portENB)
+        {
+            max = portENB;
+        }
+        else{
+            if(min > portENB)
+                min = portENB;
+        }
+        id = 0;
+        id |= (unsigned short)count;
+        id |= ((unsigned short)min << 16);
+        id |= ((unsigned short)max << 32);
+        return id;
+    }
 
     return id;
 }
