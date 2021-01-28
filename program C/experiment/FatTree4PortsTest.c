e1c64daeab861bc416987cfd386d1cc915cf8f5c
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-01-29 00:58:39 +0700

    fix bug segment fault as we have only one param

--- program C/experiment/FatTree4PortsTest.c
+++ program C/experiment/FatTree4PortsTest.c
@@ -27,8 +27,9 @@ int main(int argc, char** argv) {
     if(argc >= 2)
     {
         k = atoi(argv[1]);
-        defaultSec = atoi(argv[2]);
-        if(argc >= 3)
+        if(argc > 2)
+          defaultSec = atoi(argv[2]);
+        if(argc > 3)
             defaultBias = atoi(argv[3]);
     }
     unsigned long currentTime = 0;
