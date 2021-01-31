a90e2cfd18e41f70fba1a6f7e998153e8db7ab84
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-01-31 21:15:50 +0700

    code for controlling flows

---
+++ program C/experiment/ControlFlow.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "NetworkNode.c"
+#include "ShiftBits.c"
+
+#ifndef _CONTROL_FLOW_
+#define _CONTROL_FLOW_
+int chooseENB_ID(int EXB_ID, BufferSwitch *bufferSwitch){
+    StoredPacket *EXB = bufferSwitch->EXB[EXB_ID];
+    int found = 0;
+    int j = bufferSwitch->firstLastEXBs[EXB_ID][1];
+    j = (j == -1) ? 0 : ((j + 1) % BUFFER_SIZE);
+    if(EXB[j].dstIP == -1 
+        && EXB[j].srcIP == -1 
+        && EXB[j].id != -1){
+        found = 1;
+    }
+    int count = getCount(EXB[j].id);
+    int min = getMin(EXB[j].id);
+    int max = getMax(EXB[j].id);
+
+    int m = min;
+    unsigned long soonestPkt = 
+                    bufferSwitch->ENB[min][0].generatedTime;
+    int pickUp = min;
+    if(count > 2){
+        for(m = min; m <= max; m++){
+            if(bufferSwitch->requestedTimeOfENB[m] 
+                    == EXB[j].requestedTime
+                && 
+                    bufferSwitch->registeredEXBs[m]
+                    == EXB_ID
+                    )
+            {
+                if(soonestPkt > 
+                        bufferSwitch->ENB[m][0].generatedTime){
+                    soonestPkt = 
+                        bufferSwitch->ENB[m][0].generatedTime;
+                    pickUp = m;
+                } 
+                //countRequestedTime++;
+            }
+        }
+    }
+    else if(count == 2){
+        pickUp =
+            (bufferSwitch->ENB[max][0].generatedTime
+                < bufferSwitch->ENB[min][0].generatedTime)
+                ? max : min;
+    }
+    
+              
+}
+#endif
