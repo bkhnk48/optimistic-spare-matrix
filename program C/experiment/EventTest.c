44090f909e8ad5f5adc12ff7dac9f713e5a072e3
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-02-02 02:16:05 +0700

    add new assert

--- program C/experiment/EventTest.c
+++ program C/experiment/EventTest.c
@@ -206,6 +206,7 @@ int main(int argc, char** argv) {
                 }
               }
               #pragma endregion
+              assert(allNodes[idPrevHost].links[0].pkt->id == -1);
             }
             else if(type == E){
               #pragma region action of Event type E
