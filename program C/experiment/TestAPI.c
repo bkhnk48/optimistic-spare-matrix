5fd754734952a3bcb20750fbaab584318f9eead0
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-02-02 23:23:39 +0700

    finish getCount and getPacketsInHost

--- program C/experiment/TestAPI.c
+++ program C/experiment/TestAPI.c
@@ -5,22 +5,31 @@
 #ifndef _TEST_API_
 #define _TEST_API_
 
-unsigned long getPacketOfHost(unsigned long idHost, BufferHost *buffHost){
-    unsigned long count = 0;
-    unsigned long countSQ = 0;
-    if(buffHost->firstSQ == -1){
-        countSQ = 0;
+unsigned long getCount(unsigned long first, unsigned long last){
+    int result = 0;
+    if(first == -1){
+        result = 0;
     }
     else{
-        if(buffHost->lastSQ == -1){
-            countSQ = 1;
+        if(last == -1){
+            result = 1;
         }
         else{
-            countSQ = buffHost->firstSQ - buffHost->lastSQ + 1;
+            result = first - last + 1;
         }
     }
+    return result;
+}
 
+unsigned long getPacketsInHost(unsigned long idHost, BufferHost *buffHost){
+    unsigned long count = 0;
+    unsigned long countSQ = 
+            getCount(buffHost->firstSQ, buffHost->lastSQ);
     
+    unsigned long countEXB = 
+            getCount(buffHost->firstEXB, buffHost->lastEXB);
+    
+    count = countEXB + countSQ;
     return count;
 }
 
