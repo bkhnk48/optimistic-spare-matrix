31a33bea20743dd412d99080f85dfdb0abe23cf1
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-02-03 09:39:48 +0700

    finish the checkGeneratedPackets but not received ones in destinations

--- program C/experiment/TestAPI.c
+++ program C/experiment/TestAPI.c
@@ -74,4 +74,45 @@ void checkENB_EXB(BufferSwitch *buffSwitch, int k){
         }
     }
 }
+
+void checkGeneratedPackets(NetworkNode *allNodes, 
+                            BufferHost *buffHosts,
+                            BufferSwitch *buffSwitches,
+                            int numOfHosts,
+                            int numOfSwitches,
+                            int numOfPorts
+                            ){
+    /*
+    * This func checks all generated Packets from a source
+    * to validate whether the number is sum of 
+    * packets in hosts, packets in switches (which belong to source) 
+    * and packets in links (which belong to source)
+    * this func has not yet counted received packets in destination(s)
+    */                        
+    int i = 0, j = 0, l = 0;
+    unsigned long allGeneratedPackets = 0;
+    unsigned long pktsInHost = 0;
+    unsigned long pktsInSwitches = 0;
+    unsigned long pktsInLinks = 0;
+    for(i = 0; i < numOfHosts; i++){
+        allGeneratedPackets = allNodes[i].generatedPackets;
+        pktsInHost = getPacketsInSource(&buffHosts[i]);
+        pktsInSwitches = 0;
+        pktsInLinks = (allNodes[i].links[0].pkt->dstIP != -1) ? 1 : 0; 
+        for(j = 0; j < numOfSwitches; j++){
+            pktsInSwitches += getPacketsInSwitch(allNodes[i].ipv4
+                    , &buffSwitches[j], numOfPorts);
+            for(l = 0; l < numOfPorts; l++){
+                pktsInLinks += 
+                    (allNodes[j + numOfHosts].links[l].pkt->dstIP != -1
+                    && allNodes[j + numOfHosts].links[l].pkt->srcIP == allNodes[i].ipv4
+                        ) ? 1 : 0;
+            }
+        }
+        //printf("validated %d: %ld = %ld + %ld + %ld\n",
+        //         i, allGeneratedPackets, pktsInHost, pktsInLinks, pktsInSwitches);
+        assert(allGeneratedPackets == pktsInHost + pktsInLinks + pktsInSwitches);
+        
+    }                            
+}
 #endif
