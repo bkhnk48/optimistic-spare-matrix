f2384d95386e54cd0d903512cc1ef8754db29436
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-03-04 17:19:34 +0700

    add some funcs. To be continued...

--- program C/experiment/lib/ACalendarQueue.c
+++ program C/experiment/lib/ACalendarQueue.c
@@ -8,6 +8,16 @@
 
 unsigned long *data; //[384];
 unsigned int **arr; //[384][5];
+int width;
+unsigned long nbuckets;
+//int firstsub;
+int resizeenable;
+unsigned long qsize;
+unsigned long lastprio;
+unsigned long lastbucket;
+double buckettop;
+unsigned long bot_threshold;
+unsigned long top_threshold;
 
 void insert(int type, int idElementInGroup,
                 int portID, 
@@ -25,3 +35,59 @@ void enqueue(int type, int idElementInGroup,
                 unsigned int idNewNode);
 int dequeue();
 void printBuckets();
+
+void resize(unsigned long newsize){
+    unsigned long bwidth;
+    
+    unsigned long oldnbuckets;
+    Node** oldbuckets;
+
+    if(!resizeenable) return;
+
+    bwidth = newwidth();
+    //oldbuckets = buckets;
+    oldnbuckets = nbuckets;
+
+    localInit(newsize,bwidth,lastprio);
+
+    unsigned long i;
+    // them lai cac phan tu vao calendar moi
+    for(i=0; i < oldnbuckets; i++){
+        Node* foo = oldbuckets[i];
+        while(foo!=NULL){ // tranh viec lap vo han
+            Node* tmp = new_node(foo->type,foo->idElementInGroup,
+                                            foo->portID,foo->endTime);
+            //insert(tmp);
+            foo = foo->next;
+        }
+    }
+
+    return;
+}
+
+
+void localInit(unsigned long nbuck, unsigned long bwidth, unsigned long startprio){
+    unsigned long i;
+    double n;
+
+    width = bwidth;
+    nbuckets = nbuck;
+
+    // khoi tao cac bucket
+    qsize = 0;
+
+    // khoi tao cac chi so ban dau cua bucket dau tien
+    lastprio = startprio;
+    n = startprio / width;
+    lastbucket = ((unsigned long)n) % nbuckets;
+    buckettop = (n+1)*width + 0.5*width;
+
+    // khoi tao 2 linh canh dau vao cuoi
+    bot_threshold = nbuckets/2 - 2;
+    top_threshold = 2 * nbuckets;
+}
+
+void initqueue(){
+    localInit(2,1,0.0);
+    resizeenable = 1;
+}
