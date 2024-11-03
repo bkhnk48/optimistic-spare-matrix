8f2b2b4a3129feff1662e8bc8125bdeeab35df54
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-11-04 05:05:44 +0700

    how to use timing function

--- timing.c
+++ timing.c
@@ -16,3 +16,28 @@ void timing(double* wcTime, double* cpuTime)
    getrusage(RUSAGE_SELF, &ruse);
    *cpuTime=(double)(ruse.ru_utime.tv_sec+ruse.ru_utime.tv_usec / 1000000.0);
 }
+
+/*
+void initArray(int k)
+{
+    double wc1 = 0, wc2 = 0, cpuT = 0;
+ 
+    timing(&wc1, &cpuT);
+
+    int arr1[320][7];
+    for(i = 0; i < 320; i++)
+    {
+        for(j = 0; j < 7; j++)
+        {
+            arr1[i][j] = 0;
+        }
+    }
+
+    timing(&wc2, &cpuT);
+    printf("Time: %f ms\n", (wc2 - wc1)*1000);
+    printf("================================\n");
+    double loadedBytes = (double)7*4*sizeArr/(1000*1000);
+    printf("Performance %lf MB/s\n", loadedBytes/(wc2 - wc1));
+    return;
+}
+*/
